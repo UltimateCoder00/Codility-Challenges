0d718050ca527434e6fcc119d647c268352acc3c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-12 15:28:39 +0100

    Refector using regex

--- Lesson 1 Iterations/lib/binary_gap.rb
+++ Lesson 1 Iterations/lib/binary_gap.rb
@@ -1,16 +1,7 @@
 def binary_gap(number)
-  number = number.to_s(2).split('')
-
-  number.length.times do
-    if number[-1] == "0"
-      number = number[0..-2]
-    else
-      break
-    end
-  end
-
   array = []
-  number.chunk { |n|
+
+  number.to_s(2).sub(/0+$/,'').split('').chunk { |n|
     n == '0'
   }.each { |x| array.push(x[1].count) if x[0] == true }
 
