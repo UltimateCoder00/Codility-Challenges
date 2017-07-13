1f630fe1bcb13fe631d63a9ebe58950c50723567
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-13 15:26:59 +0100

    Add correctness tests for simple case 1 & 2

--- Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
+++ Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
@@ -7,4 +7,20 @@ describe 'Odd Occurrences In Array' do
       expect(odd_occurrences_in_array(array)).to eq 7
     end
   end
+
+  describe 'Correctness tests' do
+    context 'simple1' do
+      it '[2, 5, 8, 2, 5] to 8' do
+        array = [2, 5, 8, 2, 5]
+        expect(odd_occurrences_in_array(array)).to eq 8
+      end
+    end
+
+    context 'simple1' do
+      it '[13, 1, 55, 23, 67, 13, 55, 432, 1, 23, 67] to 3' do
+        array = [13, 1, 55, 23, 67, 13, 55, 432, 1, 23, 67]
+        expect(odd_occurrences_in_array(array)).to eq 432
+      end
+    end
+  end
 end
