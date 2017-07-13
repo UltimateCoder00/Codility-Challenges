33ff060a9546af569317d6ba772f519241d5a83c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-13 14:57:27 +0100

    Add example test

--- Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
+++ Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
@@ -1,5 +1,10 @@
 require 'odd_occurrences_in_array'
 
 describe 'Odd Occurrences In Array' do
-  
+  describe 'Example Tests' do
+    it 'example: [9, 3, 9, 3, 9, 7, 9] to 7' do
+      array = [9, 3, 9, 3, 9, 7, 9]
+      expect(odd_occurrences_in_array(array)).to eq 7
+    end
+  end
 end
