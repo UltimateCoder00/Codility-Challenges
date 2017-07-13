93172a0c0914a0ca6c5fd9cd200648eb8ee01ef5
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-13 16:29:24 +0100

    Add example test

--- Lesson 3 Time Complexity/Frogjmp/spec/frog_jump_spec.rb
+++ Lesson 3 Time Complexity/Frogjmp/spec/frog_jump_spec.rb
@@ -1,5 +1,9 @@
 require 'frog_jump'
 
 describe 'Frog Jump' do
-
+  describe 'Example Tests' do
+    it 'example: (10, 85, 30)) to 3' do
+      expect(frog_jump(10, 85, 30)).to eq 3
+    end
+  end
 end
