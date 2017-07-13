d2152d59de386ed0fba8c8d1834c47943bc5816c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-13 16:39:09 +0100

    Add correctness tests

--- Lesson 3 Time Complexity/Frogjmp/spec/frog_jump_spec.rb
+++ Lesson 3 Time Complexity/Frogjmp/spec/frog_jump_spec.rb
@@ -6,4 +6,30 @@ describe 'Frog Jump' do
       expect(frog_jump(10, 85, 30)).to eq 3
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'simple1' do
+      it 'example: (10, 100, 10)) to 9' do
+        expect(frog_jump(10, 100, 10)).to eq 9
+      end
+    end
+
+    context 'simple2' do
+      it 'example: (5, 75, 10)) to 7' do
+        expect(frog_jump(5, 75, 10)).to eq 7
+      end
+    end
+
+    context 'extreme_position - no jump needed' do
+      it 'example: (50, 50, 5)) to 0' do
+        expect(frog_jump(50, 50, 5)).to eq 0
+      end
+    end
+
+    context 'small_extreme_jump - one big jump' do
+      it 'example: (50, 500, 1000)) to 1' do
+        expect(frog_jump(50, 500, 1000)).to eq 1
+      end
+    end
+  end
 end
