6f4ca19ba97e4c94653e06c098e77c9cec8467c3
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-13 15:33:12 +0100

    Add extreme_single_item, small1 and small2 tests

--- Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
+++ Lesson 2 Arrays/OddOccurrencesInArray/spec/odd_occurrences_in_array_spec.rb
@@ -9,17 +9,46 @@ describe 'Odd Occurrences In Array' do
   end
 
   describe 'Correctness tests' do
-    context 'simple1' do
+    context 'simple1 - simple test n=5' do
       it '[2, 5, 8, 2, 5] to 8' do
         array = [2, 5, 8, 2, 5]
         expect(odd_occurrences_in_array(array)).to eq 8
       end
     end
 
-    context 'simple1' do
+    context 'simple2 simple test n=11' do
       it '[13, 1, 55, 23, 67, 13, 55, 432, 1, 23, 67] to 3' do
         array = [13, 1, 55, 23, 67, 13, 55, 432, 1, 23, 67]
         expect(odd_occurrences_in_array(array)).to eq 432
+      end
+    end
+
+    context 'extreme_single_item' do
+      it '[42] to 42' do
+        array = [42]
+        expect(odd_occurrences_in_array(array)).to eq 42
+      end
+    end
+
+    context 'small1 - small random test n=201' do
+      it 'Random Array with n=201' do
+        array = Array.new(100) { rand(1...1000000000) }
+        array = array + array
+        random_number = rand(1...1000000000)
+        array << random_number
+        array.shuffle
+        expect(odd_occurrences_in_array(array)).to eq random_number
+      end
+    end
+
+    context 'small2- small random test n=601' do
+      it 'Random Array with n=601' do
+        array = Array.new(300) { rand(1...1000000000) }
+        array = array + array
+        random_number = rand(1...1000000000)
+        array << random_number
+        array.shuffle
+        expect(odd_occurrences_in_array(array)).to eq random_number
       end
     end
   end
