69c24203c843188b7a490a1a2fdf189f453f9596
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 12:22:14 +0100

    Add code

--- Lesson 3 Time Complexity/TapeEquilibrium/lib/tape_equilibrium.rb
+++ Lesson 3 Time Complexity/TapeEquilibrium/lib/tape_equilibrium.rb
@@ -1,3 +1,11 @@
 def tape_equilibrium(a)
+  array = []
 
+  for i in 0...(a.length-1)
+    array << [a[0..i].inject(0, &:+), a[(i+1..-1)].inject(0, &:+)]
+  end
+
+  array.map! { |x| (x[0] - x[1]).abs }
+
+  array.min
 end
