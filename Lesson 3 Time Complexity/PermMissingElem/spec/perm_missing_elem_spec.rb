a2a5a1087222a47b7839cf6e49c9d4394f1628a4
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 11:14:35 +0100

    Add correctness tests

--- Lesson 3 Time Complexity/PermMissingElem/spec/perm_missing_elem_spec.rb
+++ Lesson 3 Time Complexity/PermMissingElem/spec/perm_missing_elem_spec.rb
@@ -6,4 +6,32 @@ describe 'Missing Element In Permutation' do
       expect(perm_missing_elem([2, 3, 1, 5])).to eq 4
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'empty_and_single - empty list and single element' do
+      it 'example: [] to ' do
+        expect(perm_missing_elem([])).to eq 1
+      end
+
+      it 'example: [2] to 1' do
+        expect(perm_missing_elem([2])).to eq 1
+      end
+    end
+
+    context 'missing_first_or_last - the first or the last element is missing' do
+
+    end
+
+    context 'single - single element' do
+
+    end
+
+    context 'double - two elements' do
+
+    end
+
+    context 'simple - simple test' do
+
+    end
+  end
 end
