b3f3575e971160bc61eac84ac90b7987f358ea73
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 13:41:02 +0100

    Refactor

--- Lesson 3 Time Complexity/TapeEquilibrium/lib/tape_equilibrium.rb
+++ Lesson 3 Time Complexity/TapeEquilibrium/lib/tape_equilibrium.rb
@@ -1,18 +1,15 @@
 def tape_equilibrium(a)
   array = []
+
   part1_sum = 0
   part2_sum = a.inject(0, :+)
 
-  a.each_with_index do |value, index|
-    part1_sum += value
-    part2_sum -= value
+  for i in 0...a.length
+    part1_sum += a[i]
+    part2_sum -= a[i]
 
     array << [part1_sum, part2_sum]
   end
 
-  array.pop
-
-  array.map! { |x| (x[0] - x[1]).abs }
-
-  array.min
+  array.map! { |x| (x[0] - x[1]).abs }.min
 end
