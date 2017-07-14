8c19ef13ae960564288a2b5a8dc18900c3edaa1c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 11:09:40 +0100

    Add example test

--- Lesson 3 Time Complexity/PermMissingElem/spec/perm_missing_elem_spec.rb
+++ Lesson 3 Time Complexity/PermMissingElem/spec/perm_missing_elem_spec.rb
@@ -1,5 +1,9 @@
 require 'perm_missing_elem'
 
 describe 'Missing Element In Permutation' do
-
+  describe 'Example Tests' do
+    it 'example: [2, 3, 1, 5] to 4' do
+      expect(perm_missing_elem([2, 3, 1, 5])).to eq 4
+    end
+  end
 end
