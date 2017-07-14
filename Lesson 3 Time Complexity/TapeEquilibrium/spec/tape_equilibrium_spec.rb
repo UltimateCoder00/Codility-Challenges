15d5d38f5dd183b13c50001a5e0f2b7fa3813eee
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 12:01:56 +0100

    Add example test

--- Lesson 3 Time Complexity/TapeEquilibrium/spec/tape_equilibrium_spec.rb
+++ Lesson 3 Time Complexity/TapeEquilibrium/spec/tape_equilibrium_spec.rb
@@ -1,5 +1,9 @@
 require 'tape_equilibrium'
 
 describe 'Tape Equilibrium' do
-
+  describe 'Example Tests' do
+    it 'example: [3, 1, 2, 4, 3] to 1' do
+      expect(tape_equilibrium([3, 1, 2, 4, 3])).to eq 1
+    end
+  end
 end
