5eac6ed8e66fe7abbcefcde6344aa741a470736b
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-14 12:28:04 +0100

    Add correctness tests context details

--- Lesson 3 Time Complexity/TapeEquilibrium/spec/tape_equilibrium_spec.rb
+++ Lesson 3 Time Complexity/TapeEquilibrium/spec/tape_equilibrium_spec.rb
@@ -1,9 +1,35 @@
 require 'tape_equilibrium'
 
-describe 'Tape Equilibrium' do
-  describe 'Example Tests' do
-    it 'example: [3, 1, 2, 4, 3] to 1' do
-      expect(tape_equilibrium([3, 1, 2, 4, 3])).to eq 1
+  describe 'Tape Equilibrium' do
+    describe 'Example Tests' do
+      it 'example: [3, 1, 2, 4, 3] to 1' do
+        expect(tape_equilibrium([3, 1, 2, 4, 3])).to eq 1
+      end
+    end
+
+  describe 'Correctness Tests' do
+    context 'double - two elements' do
+
+    end
+
+    context 'simple_positive - simple test with positive numbers, length = 5' do
+
+    end
+
+    context 'simple_negative - simple test with negative numbers, length = 5' do
+
+    end
+
+    context 'small_random - random small, length = 100' do
+
+    end
+
+    context 'small_range - range sequence, length = ~1,000' do
+
+    end
+
+    context 'small - small elements' do
+
     end
   end
 end
