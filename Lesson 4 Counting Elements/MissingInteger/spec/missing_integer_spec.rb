1e6c32f683cd0a3eb8c467510159a6337ce25093
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 11:47:28 +0100

    Add exmaple test

--- Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
+++ Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
@@ -1,8 +1,9 @@
 require 'missing_integer'
 
-  describe 'Missing Integer' do
-    describe 'Example Tests' do
-      it '' do
-        expect(missing_integer()).to eq 
-      end
+describe 'Missing Integer' do
+  describe 'Example Tests' do
+    it '' do
+      expect(missing_integer([1, 3, 6, 4, 1, 2])).to eq 5
     end
+  end
+end
