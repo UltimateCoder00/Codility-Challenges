a32a122aec3fef548f72db5c6db4c783819a2009
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 12:49:38 +0100

    Refactor

--- Lesson 4 Counting Elements/MissingInteger/lib/missing_integer.rb
+++ Lesson 4 Counting Elements/MissingInteger/lib/missing_integer.rb
@@ -1,13 +1,10 @@
 def missing_integer(a)
-  a = a.uniq.sort
-
   a.select! { |x| x > 0 }
-
   return 1 if a.empty?
 
-  for i in 1..a.length
+  a.sort!.uniq!
+
+  for i in 1..(a.length+1)
     return i if a[i-1] != i
   end
-
-  return a.length + 1
 end
