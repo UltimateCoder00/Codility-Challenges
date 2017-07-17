6c5ed5b4289daa34a585e465344ba74540f85a24
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 17:53:54 +0100

    Add simple tests

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -2,8 +2,12 @@ require 'permutation_check'
 
 describe 'Permutation Check' do
   describe 'Example Tests' do
-    it '' do
-      expect(permutation_check()).to eq
+    it 'Test 1' do
+      expect(permutation_check([4, 1, 3, 2])).to eq 1
+    end
+
+    it 'Test 2' do
+      expect(permutation_check([4, 1, 3])).to eq 0
     end
   end
 end
