ded296162dd7912482ac40c485a6967a0c1d86e9
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 12:04:06 +0100

    Add shuffled sequence of 0...100 and then 102...200 array test

--- Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
+++ Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
@@ -2,8 +2,18 @@ require 'missing_integer'
 
 describe 'Missing Integer' do
   describe 'Example Tests' do
-    it '' do
+    it 'example (without minus) - [1, 3, 6, 4, 1, 2] to 5' do
       expect(missing_integer([1, 3, 6, 4, 1, 2])).to eq 5
+    end
+  end
+
+  describe 'Example Tests' do
+    context 'positive_only' do
+      it 'shuffled sequence of 0...100 and then 102...200' do
+        array = [*0..100, *102..200]
+        array.shuffle
+        expect(missing_integer(array)).to eq 101
+      end
     end
   end
 end
