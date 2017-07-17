c20f6e26cf273971ae082034b290d2beca8565d5
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 12:21:04 +0100

    Add more Correctness tests

--- Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
+++ Lesson 4 Counting Elements/MissingInteger/spec/missing_integer_spec.rb
@@ -7,12 +7,52 @@ describe 'Missing Integer' do
     end
   end
 
-  describe 'Example Tests' do
+  describe 'Correctness Tests' do
+    context 'extreme_single' do
+      it 'a single element' do
+        expect(missing_integer([0])).to eq 1
+        expect(missing_integer([-1])).to eq 1
+        expect(missing_integer([1])).to eq 2
+        expect(missing_integer([-2147483647])).to eq 1
+        expect(missing_integer([2147483647])).to eq 1
+      end
+    end
+
+    context 'simple' do
+      it 'simple test' do
+        array = [2]
+        expect(missing_integer(array)).to eq 1
+
+        array = [1, 1, 3]
+        expect(missing_integer(array)).to eq 2
+
+        array = [3, 4, 2, 7]
+        expect(missing_integer(array)).to eq 1
+
+        array = [3, 1, 2, 5, 6]
+        expect(missing_integer(array)).to eq 4
+      end
+    end
+
+    context 'extreme_min_max_int' do
+      it 'MININT and MAXINT (with minus)' do
+
+      end
+    end
+
     context 'positive_only' do
       it 'shuffled sequence of 0...100 and then 102...200' do
         array = [*0..100, *102..200]
         array.shuffle
         expect(missing_integer(array)).to eq 101
+      end
+    end
+
+    context 'negative_only' do
+      it 'shuffled sequence -100 ... -1' do
+        array = [*-100..-1]
+        array.shuffle
+        expect(missing_integer(array)).to eq 1
       end
     end
   end
