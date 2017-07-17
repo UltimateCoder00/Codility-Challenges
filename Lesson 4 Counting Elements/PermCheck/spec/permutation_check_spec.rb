6d7b1b58b75f276340f0b0377216733a81aaa224
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 17:59:36 +0100

    modify example tests

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -2,12 +2,16 @@ require 'permutation_check'
 
 describe 'Permutation Check' do
   describe 'Example Tests' do
-    it 'Test 1' do
-      expect(permutation_check([4, 1, 3, 2])).to eq 1
+    context 'example1' do
+      it 'the first example test - [4, 1, 3, 2]' do
+        expect(permutation_check([4, 1, 3, 2])).to eq 1
+      end
     end
 
-    it 'Test 2' do
-      expect(permutation_check([4, 1, 3])).to eq 0
+    context 'example2' do
+      it 'the second example test - [4, 1, 3]' do
+        expect(permutation_check([4, 1, 3])).to eq 0
+      end
     end
   end
 end
