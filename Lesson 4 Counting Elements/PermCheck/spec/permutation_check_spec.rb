a8b80a922d3cbeebd199cd82731ff925cfb11a26
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 18:14:37 +0100

    Add performance test context details

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -13,55 +13,87 @@ describe 'Permutation Check' do
         expect(permutation_check([4, 1, 3])).to eq 0
       end
     end
+  end
 
-    describe 'Correctness Tests' do
-      context 'extreme_min_max' do
-        it 'single element with minimal/maximal value' do
-          expect(permutation_check([1])).to eq 1
-          expect(permutation_check([1000000000])).to eq 0
-        end
+  describe 'Correctness Tests' do
+    context 'extreme_min_max' do
+      it 'single element with minimal/maximal value' do
+        expect(permutation_check([1])).to eq 1
+        expect(permutation_check([1000000000])).to eq 0
       end
+    end
 
-      context 'single' do
-        it 'single element' do
-          expect(permutation_check([456])).to eq 0
-          expect(permutation_check([3457347])).to eq 0
-        end
+    context 'single' do
+      it 'single element' do
+        expect(permutation_check([456])).to eq 0
+        expect(permutation_check([3457347])).to eq 0
       end
+    end
 
-      context 'double' do
-        it 'two elements' do
-          expect(permutation_check([1, 2])).to eq 1
-          expect(permutation_check([2, 1])).to eq 1
-          expect(permutation_check([1, 4])).to eq 0
-          expect(permutation_check([2, 3])).to eq 0
-        end
+    context 'double' do
+      it 'two elements' do
+        expect(permutation_check([1, 2])).to eq 1
+        expect(permutation_check([2, 1])).to eq 1
+        expect(permutation_check([1, 4])).to eq 0
+        expect(permutation_check([2, 3])).to eq 0
       end
+    end
 
-      context 'antiSum1' do
-        it 'total sum is correct, but it is not a permutation, N <= 10' do
-          expect(permutation_check([1, 2 , 3, 5, 5, 5, 7, 8, 9, 10])).to eq 0
+    context 'antiSum1' do
+      it 'total sum is correct, but it is not a permutation, N <= 10' do
+        expect(permutation_check([1, 2 , 3, 5, 5, 5, 7, 8, 9, 10])).to eq 0
 
-          expect(permutation_check([2, 2, 2])).to eq 0
+        expect(permutation_check([2, 2, 2])).to eq 0
 
-          expect(permutation_check([1, 5, 4, 5])).to eq 0
+        expect(permutation_check([1, 5, 4, 5])).to eq 0
 
-          expect(permutation_check([6, 4, 5, 6])).to eq 0
-        end
+        expect(permutation_check([6, 4, 5, 6])).to eq 0
       end
+    end
 
-      context 'small_permutation' do
-        it 'permutation + one element occurs twice, N = ~100' do
-          array = [*1..100]
-          array << (rand(100) + 1)
-          array.shuffle
-          expect(permutation_check(array)).to eq 0
+    context 'small_permutation' do
+      it 'permutation + one element occurs twice, N = ~100' do
+        array = [*1..100]
+        array << (rand(100) + 1)
+        array.shuffle
+        expect(permutation_check(array)).to eq 0
 
-          array = [*1..105]
-          array << (rand(105) + 1)
-          array.shuffle
-          expect(permutation_check(array)).to eq 0
-        end
+        array = [*1..105]
+        array << (rand(105) + 1)
+        array.shuffle
+        expect(permutation_check(array)).to eq 0
+      end
+    end
+  end
+
+  describe 'Performance Tests' do
+    context 'medium_permutation' do
+      it 'permutation + few elements occur twice, N = ~10,000' do
+
+      end
+    end
+
+    context 'antiSum2' do
+      it 'total sum is correct, but it is not a permutation, N = ~100,000' do
+
+      end
+    end
+
+    context 'large_permutation' do
+      it 'permutation + one element occurs three times, N = ~100,000' do
+
+      end
+    end
+
+    context 'large_range' do
+      it 'sequence 1, 2, ..., N, N = ~100,000' do
+
+      end
+    end
+
+    context 'extreme_values' do
+      it 'all the same values, N = ~100,000' do
+
       end
     end
   end
