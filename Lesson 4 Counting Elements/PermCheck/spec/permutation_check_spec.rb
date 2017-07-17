3d6fedc5c2de8872ffcad1d16862fc47d292ff78
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 18:03:16 +0100

    Add performance test context details

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -13,5 +13,37 @@ describe 'Permutation Check' do
         expect(permutation_check([4, 1, 3])).to eq 0
       end
     end
+
+    describe 'Correctness Tests' do
+      context 'extreme_min_max' do
+        it 'single element with minimal/maximal value' do
+
+        end
+      end
+
+      context 'single' do
+        it 'single element' do
+
+        end
+      end
+
+      context 'double' do
+        it 'two elements' do
+
+        end
+      end
+
+      context 'antiSum1' do
+        it 'total sum is correct, but it is not a permutation, N <= 10' do
+
+        end
+      end
+
+      context 'small_permutation' do
+        it 'permutation + one element occurs twice, N = ~100' do
+
+        end
+      end
+    end
   end
 end
