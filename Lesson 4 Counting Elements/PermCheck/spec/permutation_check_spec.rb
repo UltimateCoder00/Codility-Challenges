08b3b4ba3140cd7ddc82760f23dac3cb5970600a
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 18:28:09 +0100

    Add performance tests

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -69,31 +69,91 @@ describe 'Permutation Check' do
   describe 'Performance Tests' do
     context 'medium_permutation' do
       it 'permutation + few elements occur twice, N = ~10,000' do
+        array = [*1..10000]
+        array << (rand(10000) + 1)
+        array << (rand(10000) + 1)
+        array << (rand(10000) + 1)
+        array << (rand(10000) + 1)
+        array << (rand(10000) + 1)
+        array.shuffle
+        expect(permutation_check(array)).to eq 0
 
+        array2 = [*1..10000]
+        array2 << (rand(10000) + 1)
+        array2 << (rand(10000) + 1)
+        array2 << (rand(10000) + 1)
+        array2 << (rand(10000) + 1)
+        array2 << (rand(10000) + 1)
+        array2.shuffle
+        expect(permutation_check(array2)).to eq 0
       end
     end
 
     context 'antiSum2' do
       it 'total sum is correct, but it is not a permutation, N = ~100,000' do
+        array = [*1..100000]
+        random_number = rand(90000)
+        array << array[random_number + 1]
+        array << array[random_number + 1]
+        array.delete_at(random_number)
+        array.delete_at(random_number+1)
+        array.shuffle
+        expect(permutation_check(array)).to eq 0
 
+        array2 = [*1..100000]
+        random_number2 = rand(90000)
+        array2 << array2[random_number2 + 1]
+        array2 << array2[random_number2 + 1]
+        array2.delete_at(random_number2)
+        array2.delete_at(random_number2+1)
+        array2.shuffle
+        expect(permutation_check(array2)).to eq 0
       end
     end
 
     context 'large_permutation' do
       it 'permutation + one element occurs three times, N = ~100,000' do
+        array = [*1..100000]
+        random_number = (rand(100000) + 1)
+        array << random_number
+        array << random_number
+        array << random_number
+        array.shuffle
+        expect(permutation_check(array)).to eq 0
 
+        array2 = [*1..100000]
+        random_number2 = (rand(100000) + 1)
+        array2 << random_number2
+        array2 << random_number2
+        array2 << random_number2
+        array2.shuffle
+        expect(permutation_check(array2)).to eq 0
       end
     end
 
     context 'large_range' do
       it 'sequence 1, 2, ..., N, N = ~100,000' do
+        array = [*1..90000]
+        expect(permutation_check(array)).to eq 1
 
+        arrays = [*1..100000]
+        expect(permutation_check(arrays)).to eq 1
       end
     end
 
     context 'extreme_values' do
       it 'all the same values, N = ~100,000' do
+        random_number = (rand(100000) + 1)
+        array = [random_number]*100000
+        expect(permutation_check(array)).to eq 0
 
+        random_number2 = (rand(100000) + 1)
+        array2 = [random_number2]*100000
+        expect(permutation_check(array2)).to eq 0
+
+        random_number3 = (rand(100000) + 1)
+        array3 = [random_number3]*100000
+        expect(permutation_check(array3)).to eq 0
       end
     end
   end
