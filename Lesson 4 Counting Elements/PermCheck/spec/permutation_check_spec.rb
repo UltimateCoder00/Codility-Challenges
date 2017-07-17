39de15c142dd5acd679a535be3a8facfe56a751c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-17 18:11:21 +0100

    Add correctness tests

--- Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
+++ Lesson 4 Counting Elements/PermCheck/spec/permutation_check_spec.rb
@@ -17,31 +17,50 @@ describe 'Permutation Check' do
     describe 'Correctness Tests' do
       context 'extreme_min_max' do
         it 'single element with minimal/maximal value' do
-
+          expect(permutation_check([1])).to eq 1
+          expect(permutation_check([1000000000])).to eq 0
         end
       end
 
       context 'single' do
         it 'single element' do
-
+          expect(permutation_check([456])).to eq 0
+          expect(permutation_check([3457347])).to eq 0
         end
       end
 
       context 'double' do
         it 'two elements' do
-
+          expect(permutation_check([1, 2])).to eq 1
+          expect(permutation_check([2, 1])).to eq 1
+          expect(permutation_check([1, 4])).to eq 0
+          expect(permutation_check([2, 3])).to eq 0
         end
       end
 
       context 'antiSum1' do
         it 'total sum is correct, but it is not a permutation, N <= 10' do
+          expect(permutation_check([1, 2 , 3, 5, 5, 5, 7, 8, 9, 10])).to eq 0
 
+          expect(permutation_check([2, 2, 2])).to eq 0
+
+          expect(permutation_check([1, 5, 4, 5])).to eq 0
+
+          expect(permutation_check([6, 4, 5, 6])).to eq 0
         end
       end
 
       context 'small_permutation' do
         it 'permutation + one element occurs twice, N = ~100' do
+          array = [*1..100]
+          array << (rand(100) + 1)
+          array.shuffle
+          expect(permutation_check(array)).to eq 0
 
+          array = [*1..105]
+          array << (rand(105) + 1)
+          array.shuffle
+          expect(permutation_check(array)).to eq 0
         end
       end
     end
