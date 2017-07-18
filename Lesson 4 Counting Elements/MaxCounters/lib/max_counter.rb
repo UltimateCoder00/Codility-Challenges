255499f5e7d19e2f62d682a837cde4b9ffb0f94c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 14:29:29 +0100

    Refactor for extreme_large performance test

--- Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
+++ Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
@@ -1,7 +1,11 @@
 def max_counter(n, a)
-  (a.length-1).downto(1) do |x|
-    if a[x] > n
-      a.delete_at(x) if a[x] == a[x-1]
+  number = a.count(n+1)
+
+  if (number / (a.size*1.0)) >= ((a.size * 0.1) / a.size)
+    (a.length-1).downto(1) do |x|
+      if a[x] > n
+        a.delete_at(x) if a[x] == a[x-1]
+      end
     end
   end
 
