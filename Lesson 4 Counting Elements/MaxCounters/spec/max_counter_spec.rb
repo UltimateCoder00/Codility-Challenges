9f307c61fc24ce582d0045b26b9a52f9b2988b60
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 13:20:27 +0100

    Add correctness tests

--- Lesson 4 Counting Elements/MaxCounters/spec/max_counter_spec.rb
+++ Lesson 4 Counting Elements/MaxCounters/spec/max_counter_spec.rb
@@ -10,25 +10,25 @@ describe 'Max Counter' do
   describe 'Correctness Tests' do
     context "extreme_small" do
       it "all max_counter operations" do
-
+        expect(max_counter(4, [5,5,5,5])).to eq [0,0,0,0]
       end
     end
 
     context "single" do
       it "only one counter" do
-
+        expect(max_counter(3, [1])).to eq [1,0,0]
       end
     end
 
     context "small_random1" do
       it "small random test, 6 max_counter operations" do
-
+        expect(max_counter(3, [1,4,4,4,4,4,4,2])).to eq [1,2,1]
       end
     end
 
     context "small_random2" do
       it "small random test, 10 max_counter operations" do
-
+        expect(max_counter(2, [1,3,2,3,3,3,3,3,3,3,3,3,1])).to eq [3,2]
       end
     end
   end
