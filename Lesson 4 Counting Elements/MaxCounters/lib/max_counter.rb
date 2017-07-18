8bd21f4bacee68de00578f8fccf0df75ed8f5747
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 17:24:03 +0100

    Extend code to pass all performance tests

--- Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
+++ Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
@@ -11,11 +11,41 @@ def max_counter(n, a)
 
   array = [0]*n
 
-  for i in 0...a.length
-    unless a[i] > n
-      array[a[i]-1] += 1
-    else
-      array.fill array.max
+  unless a[0] == n+1
+    value = 0
+
+    n_indexes = []
+    a.each_with_index do |x,i|
+      if x == n+1
+        n_indexes << [value,i - 1] if i > 0
+        n_indexes << [value,0] if i == 0
+        value = i+1
+      end
+    end
+
+    counting = []
+    counts = 0
+
+    if number > 0
+      n_indexes.each do |x|
+        temp = a[x[0]..x[1]].max_by { |k| a[x[0]..x[1]].count(k) }
+        counting << a[x[0]..x[1]].count(temp)
+      end
+
+      counting.each do |x|
+        counts += x
+        array.fill(counts)
+      end
+    end
+
+    if n_indexes.empty?
+      for i in 0...a.length
+        array[a[i]-1] += 1
+      end
+    elsif n_indexes.last[1] + 2 <= a.size
+      for i in (n_indexes.last[1] + 2)...a.length
+        array[a[i]-1] += 1
+      end
     end
   end
 
