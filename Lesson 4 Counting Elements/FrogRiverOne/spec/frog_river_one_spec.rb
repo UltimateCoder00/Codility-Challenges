140ded5e1309a685f53e62d605dd4d76f0bf6c58
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 11:13:21 +0100

    Add correctness test

--- Lesson 4 Counting Elements/FrogRiverOne/spec/frog_river_one_spec.rb
+++ Lesson 4 Counting Elements/FrogRiverOne/spec/frog_river_one_spec.rb
@@ -2,8 +2,46 @@ require 'frog_river_one'
 
 describe 'Frog River One' do
   describe 'Example Tests' do
-    it '' do
-      expect(frog_river_one()).to eq
+    it 'Example test: (5, [1, 3, 1, 4, 2, 3, 5, 4]) to 6' do
+      expect(frog_river_one(5, [1, 3, 1, 4, 2, 3, 5, 4])).to eq 6
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context "simple" do
+      it "simple test" do
+
+      end
+    end
+
+    context "single" do
+      it "single element" do
+
+      end
+    end
+
+    context "extreme_frog" do
+      it "frog never across the river" do
+
+      end
+    end
+
+    context "small_random1" do
+      it "3 random permutation, X = 50" do
+
+      end
+    end
+
+    context "small_random2" do
+      it "5 random permutation, X = 60" do
+
+      end
+    end
+
+    context "extreme_leaves" do
+      it "all leaves in the same place" do
+
+      end
     end
   end
 end
