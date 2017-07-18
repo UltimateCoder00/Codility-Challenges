428504ad5f7d6c7627960a9ac89b71668507b4e3
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 13:12:34 +0100

    Write method code

--- Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
+++ Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
@@ -1,3 +1,13 @@
 def max_counter(n, a)
+  array = [0]*n
 
+  for i in 0...a.length
+    unless a[i] > n
+      array[a[i]-1] += 1
+    else
+      array.fill array.max
+    end
+  end
+
+  array
 end
