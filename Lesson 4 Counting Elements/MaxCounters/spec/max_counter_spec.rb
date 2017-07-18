5fe93cf4f273773f334f21385d42facafb0ae2a4
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-18 13:01:50 +0100

    Add example test

--- Lesson 4 Counting Elements/MaxCounters/spec/max_counter_spec.rb
+++ Lesson 4 Counting Elements/MaxCounters/spec/max_counter_spec.rb
@@ -2,8 +2,8 @@ require 'max_counter'
 
 describe 'Max Counter' do
   describe 'Example Tests' do
-    it '' do
-      expect(max_counter()).to eq
+    it 'Example test: (5, [3,4,4,6,1,4,4]) to [3,2,2,4,2]' do
+      expect(max_counter(5, [3,4,4,6,1,4,4])).to eq [3,2,2,4,2]
     end
   end
 end
