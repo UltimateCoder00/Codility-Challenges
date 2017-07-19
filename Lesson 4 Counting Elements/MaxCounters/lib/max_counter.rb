b0d5f87000173e550f39e8e17725dfdefe58dcfb
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-19 13:39:32 +0100

    Refactor no max counter code

--- Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
+++ Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
@@ -1,3 +1,5 @@
+require 'facets'
+
 def max_counter(n, a)
   a.size.times { a[0] == n+1 ? a.shift : break  }
 
@@ -7,22 +9,24 @@ def max_counter(n, a)
 
   return array if a.empty?
 
-  max_counter_array_indices = a.each_index.select{|i| a[i] == n+1}
+  max_counters = a.each_index.select{ |i| a[i] == n+1 }
 
-  value = 0
-  n_indexes = []
+  if max_counters.empty?
+    a.frequency.sort_by(&:last).each { |element| array[element[0]-1] += element[1] }
+    
+    return array
+  end
 
-  a.each_with_index do |x,i|
-    if x == n+1
-      n_indexes << [value,i - 1]
-      value = i+1
-    end
+  n_indexes = [[0,max_counters[0]-1]]
+
+  for i in 0...(max_counters.length-1)
+    n_indexes << [max_counters[i] + 1, max_counters[i + 1] - 1]
   end
 
   counting = []
   counts = 0
 
-  unless max_counter_array_indices.empty?
+  unless max_counters.empty?
     n_indexes.each do |x|
       temp = a[x[0]..x[1]].max_by { |k| a[x[0]..x[1]].count(k) }
       counting << a[x[0]..x[1]].count(temp)
@@ -34,7 +38,7 @@ def max_counter(n, a)
     end
   end
 
-  if max_counter_array_indices.empty?
+  if max_counters.empty?
     for i in 0...a.length
       array[a[i]-1] += 1
     end
