34c0dd9e8bf8a07bced5af25b286404be9bd7be2
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-19 14:31:11 +0100

    More refactor

--- Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
+++ Lesson 4 Counting Elements/MaxCounters/lib/max_counter.rb
@@ -2,18 +2,14 @@ def max_counter(n, a)
   array = [0]*n
 
   a = remove_redundant_max_counters(n, a)
+
   return array if a.empty?
+  return increment_counters_for_array_with_no_max_counters(a, array) unless a.include?(n+1)
 
   max_counters = get_array_indices_of_max_counters(n, a)
+  max_counter_ranges = get_range_between_max_counter_elements_in_array(max_counters)
 
-  if max_counters.empty?
-    a = increment_counters_for_array_with_no_max_counters(a, array)
-    return array
-  end
-
-  max_counter_range = get_range_between_max_counter_elements_in_array(max_counters)
-
-  increment_counters(max_counter_range, array, a)
+  increment_counters(max_counter_ranges, array, a)
 end
 
 def remove_redundant_max_counters(n, a)
@@ -22,23 +18,23 @@ def remove_redundant_max_counters(n, a)
   a
 end
 
-def get_array_indices_of_max_counters(n, a)
-  a.each_index.select{ |i| a[i] == n+1 }
-end
-
 def increment_counters_for_array_with_no_max_counters(a, array)
   a.each_with_object(Hash.new(0)){ |m,h| h[m] += 1 }.sort_by{ |k,v| v }.each { |element| array[element[0]-1] += element[1] }
   array
 end
 
+def get_array_indices_of_max_counters(n, a)
+  a.each_index.select{ |i| a[i] == n+1 }
+end
+
 def get_range_between_max_counter_elements_in_array(max_counters)
-  max_counter_range = [[0,max_counters[0]-1]]
+  max_counter_ranges = [[0,max_counters[0]-1]]
 
   for i in 0...(max_counters.length-1)
-    max_counter_range << [max_counters[i]+1, max_counters[i+1]-1]
+    max_counter_ranges << [max_counters[i]+1, max_counters[i+1]-1]
   end
 
-  max_counter_range
+  max_counter_ranges
 end
 
 def increment_counters(max_counter_range, array, a)
