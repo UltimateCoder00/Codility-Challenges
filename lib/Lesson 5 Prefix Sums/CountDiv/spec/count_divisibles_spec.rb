ac2e2c2d2497be7bd397122a320596b7422da403
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-24 17:10:03 +0100

    Add example test

---
+++ lib/Lesson 5 Prefix Sums/CountDiv/spec/count_divisibles_spec.rb
@@ -0,0 +1,9 @@
+require 'count_divisibles'
+
+describe 'Count Divisibles' do
+  describe 'Example Tests' do
+    it 'example: ((6, 11, 2)) to 3' do
+      expect(count_divisibles(6, 11, 2)).to eq 3
+    end
+  end
+end
