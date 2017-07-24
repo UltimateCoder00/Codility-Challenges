1d75b0adbfcbd500b54e676a96052e37aa2b541b
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-24 18:08:56 +0100

    Refactor method

--- lib/Lesson 5 Prefix Sums/CountDiv/lib/count_divisibles.rb
+++ lib/Lesson 5 Prefix Sums/CountDiv/lib/count_divisibles.rb
@@ -1,24 +1,3 @@
 def count_divisibles(a, b, k)
-  count = 0
-
-  unless a == 0
-    return count if k > b
-  else
-    return 1 if k > b
-  end
-
-  if a < k
-    unless a == 0
-      a = k
-    else
-      a = k
-      count += 1
-    end
-  end
-
-  for i in a..b
-    count += 1 if i % k == 0
-  end
-
-  count
+  (b / k) - ((a-1) / k)
 end
