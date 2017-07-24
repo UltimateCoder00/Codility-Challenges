5f375fee431ead0db0c7f6a864559a3967d987c8
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-24 17:16:00 +0100

    Add correctness test contexts

--- lib/Lesson 5 Prefix Sums/CountDiv/spec/count_divisibles_spec.rb
+++ lib/Lesson 5 Prefix Sums/CountDiv/spec/count_divisibles_spec.rb
@@ -6,4 +6,30 @@ describe 'Count Divisibles' do
       expect(count_divisibles(6, 11, 2)).to eq 3
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'simple' do
+      it 'A = 11, B = 345, K = 17' do
+
+      end
+    end
+
+    context 'minimal'
+      it 'A = B in {0,1}, K = 11' do
+
+      end
+    end
+
+    context 'extreme_ifempty'
+      it 'A = 10, B = 10, K in {5,7,20}' do
+
+      end
+    end
+
+    context 'extreme_endpoints'
+      it 'verify handling of range endpoints, multiple runs' do
+
+      end
+    end
+  end
 end
