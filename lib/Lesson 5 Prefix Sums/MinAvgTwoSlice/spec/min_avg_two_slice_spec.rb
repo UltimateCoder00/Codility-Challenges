c633e83351844ee00c1b9d59062a570642918bbd
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 17:12:51 +0100

    Add example test

---
+++ lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/spec/min_avg_two_slice_spec.rb
@@ -0,0 +1,9 @@
+require 'min_avg_two_slice'
+
+describe 'Minimum Average of Two Slice' do
+  describe 'Example Tests' do
+    it 'example: [4,2,2,5,1,5,8] gives 1' do
+      expect(min_avg_two_slice([4,2,2,5,1,5,8])).to eq 1
+    end
+  end
+end
