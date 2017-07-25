019f1dd47d40b6121a2342ffbb124fc3de3605f9
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 15:12:20 +0100

    Add correctness test contexts

--- lib/Lesson 5 Prefix Sums/PassingCars/spec/passing_cars_spec.rb
+++ lib/Lesson 5 Prefix Sums/PassingCars/spec/passing_cars_spec.rb
@@ -6,4 +6,36 @@ describe 'Passing Cars' do
       expect(passing_cars([0,1,0,1,1])).to eq 5
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'single' do
+      it 'single element' do
+
+      end
+    end
+
+    context 'double' do
+      it 'two elements' do
+
+      end
+    end
+
+    context 'simple' do
+      it 'simple test' do
+
+      end
+    end
+
+    context 'small_random' do
+      it 'random, length = 100' do
+
+      end
+    end
+
+    context 'small_random2' do
+      it 'random, length = 1000' do
+
+      end
+    end
+  end
 end
