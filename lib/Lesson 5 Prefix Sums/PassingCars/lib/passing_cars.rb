5b898913b7287f94949613b4e62484ab12867d0f
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 15:59:49 +0100

    Refactor for array with 1's at beginning and 0's at end of array

--- lib/Lesson 5 Prefix Sums/PassingCars/lib/passing_cars.rb
+++ lib/Lesson 5 Prefix Sums/PassingCars/lib/passing_cars.rb
@@ -1,6 +1,14 @@
 def passing_cars(a)
   array = []
 
+  for i in 0..a.length
+    a[0] == 1 ? a.shift : break
+  end
+
+  for i in 0..a.length
+    a.last == 0 ? a.pop : break
+  end
+
   a.each_with_index { |element,index| array << index if element == 0}
 
   count = 0
