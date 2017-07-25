5c00b659b2f7a22d2904aa53bb639849a319fa2a
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 17:44:27 +0100

    Add method code and pass example test

--- lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/lib/min_avg_two_slice.rb
+++ lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/lib/min_avg_two_slice.rb
@@ -1,3 +1,13 @@
 def min_avg_two_slice(a)
+  array = []
+  array2 = []
 
+  for i in 0...(a.length - 1)
+    for j in (i+1)...(a.length)
+      array << (a[i..j].inject(0, :+) / (j - i + 1.0))
+      array2 << i
+    end
+  end
+
+  array2[array.index(array.min)]
 end
