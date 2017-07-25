46e4bb1d91fbb47564d519298b9fa8bf275a859d
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 17:48:27 +0100

    Add correctness test contexts

--- lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/spec/min_avg_two_slice_spec.rb
+++ lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/spec/min_avg_two_slice_spec.rb
@@ -6,4 +6,36 @@ describe 'Minimum Average of Two Slice' do
       expect(min_avg_two_slice([4,2,2,5,1,5,8])).to eq 1
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'double_quadruple' do
+      it 'two or four elements' do
+
+      end
+    end
+
+    context 'simple1' do
+      it 'simple test, the best slice has length' do
+
+      end
+    end
+
+    context 'simple2' do
+      it 'simple test, the best slice has length' do
+
+      end
+    end
+
+    context 'small_random' do
+      it 'random, length = 100' do
+
+      end
+    end
+
+    context 'medium_range' do
+      it 'increasing, decreasing (legth = ~100) and small functional' do
+
+      end
+    end
+  end
 end
