abafc6250c7d6c1957f9f5577fa9d46c57014d2d
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-25 15:06:01 +0100

    Add method code and pass example test

--- lib/Lesson 5 Prefix Sums/PassingCars/lib/passing_cars.rb
+++ lib/Lesson 5 Prefix Sums/PassingCars/lib/passing_cars.rb
@@ -1,3 +1,10 @@
 def passing_cars(a)
+  array = []
 
+  a.each_with_index { |element,index| array << index if element == 0}
+
+  count = 0
+
+  array.each_with_index { |element,index| count += a[index+1...a.size].select { |x| x == 1 }.count }
+  count
 end
