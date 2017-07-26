5e5771294dfbc01b856ea3bbb52eb36345dc02d5
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-26 13:26:15 +0100

    Add genomic_range_query(s, p, q) method code and pass example test

--- lib/Lesson 5 Prefix Sums/GenomicRangeQuery/lib/genomic_range_query.rb
+++ lib/Lesson 5 Prefix Sums/GenomicRangeQuery/lib/genomic_range_query.rb
@@ -1,3 +1,15 @@
 def genomic_range_query(s, p, q)
+  s.gsub!("A","1")
+  s.gsub!("C","2")
+  s.gsub!("G","3")
+  s.gsub!("T","4")
+  s = s.split("").map(&:to_i)
 
+  array = []
+
+  for i in 0...p.length
+    array << s[p[i]..q[i]].min
+  end
+
+  array
 end
