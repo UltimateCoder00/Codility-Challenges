699523a03ba9e2af704c25357535331b1bcb3a57
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-26 14:02:30 +0100

    Refactor

--- lib/Lesson 5 Prefix Sums/GenomicRangeQuery/lib/genomic_range_query.rb
+++ lib/Lesson 5 Prefix Sums/GenomicRangeQuery/lib/genomic_range_query.rb
@@ -3,12 +3,21 @@ def genomic_range_query(s, p, q)
   s.gsub!("C","2")
   s.gsub!("G","3")
   s.gsub!("T","4")
+
   s = s.split("").map(&:to_i)
 
   array = []
 
   for i in 0...p.length
-    array << s[p[i]..q[i]].min
+    if s[p[i]..q[i]].include?(1)
+      array << 1
+    elsif s[p[i]..q[i]].include?(2)
+      array << 2
+    elsif s[p[i]..q[i]].include?(3)
+      array << 3
+    else
+      array << 4
+    end
   end
 
   array
