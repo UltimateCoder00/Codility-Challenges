c28b4dad4f8b8b7dec7f406febadac869892d5b3
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-26 13:29:02 +0100

    Add correctness test contexts

--- lib/Lesson 5 Prefix Sums/GenomicRangeQuery/spec/genomic_range_query_spec.rb
+++ lib/Lesson 5 Prefix Sums/GenomicRangeQuery/spec/genomic_range_query_spec.rb
@@ -6,4 +6,36 @@ describe 'Genomic Range Query' do
       expect(genomic_range_query('CAGCCTA', [2,5,0], [4,5,6])).to eq [2,4,1]
     end
   end
+
+  describe 'Correctness Tests'
+    context 'extreme_sinlge' do
+      it 'single character string' do
+
+      end
+    end
+
+    context 'extreme_double' do
+      it 'double character string' do
+
+      end
+    end
+
+    context 'simple' do
+      it 'simple tests' do
+
+      end
+    end
+
+    context 'small_length_string' do
+      it 'small length simple string' do
+
+      end
+    end
+
+    context 'small_random' do
+      it 'small random string, length = ~300' do
+
+      end
+    end
+  end
 end
