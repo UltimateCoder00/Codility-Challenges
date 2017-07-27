c3663e017180f7770ed614d5c4da5a57b8e2d84e
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-27 14:11:58 +0100

    Typo: change passing_cars to distinct

--- lib/Lesson 6 Sorting/Distinct/spec/distinct_spec.rb
+++ lib/Lesson 6 Sorting/Distinct/spec/distinct_spec.rb
@@ -3,7 +3,7 @@ require 'distinct'
 describe 'Distinct' do
   describe 'Example Tests' do
     it 'example: [2, 1, 1, 2, 3, 1] gives 3' do
-      expect(passing_cars([2, 1, 1, 2, 3, 1])).to eq 3
+      expect(distinct([2, 1, 1, 2, 3, 1])).to eq 3
     end
   end
 end
