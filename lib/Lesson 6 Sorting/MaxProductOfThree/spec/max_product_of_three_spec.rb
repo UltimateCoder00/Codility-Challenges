6c690f5a4ca5925f36366f0ed14d81ab8cb5f123
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-01 15:16:31 +0100

    Add correctness test contexts

--- lib/Lesson 6 Sorting/MaxProductOfThree/spec/max_product_of_three_spec.rb
+++ lib/Lesson 6 Sorting/MaxProductOfThree/spec/max_product_of_three_spec.rb
@@ -6,4 +6,30 @@ describe 'Max Product of Three' do
       expect(max_product_of_three([-3, 1, 2, -2, 5, 6])).to eq 60
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'one_triple' do
+      it 'three elements' do
+
+      end
+    end
+
+    context 'simple1' do
+      it 'simple tests' do
+
+      end
+    end
+
+    context 'simple2' do
+      it 'simple tests' do
+
+      end
+    end
+
+    context 'small_random' do
+      it 'random small, length = 100' do
+
+      end
+    end
+  end
 end
