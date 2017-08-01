64cb62e4873543b3bc9d53602ca700e512a410f7
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-01 15:12:47 +0100

    Add method code and pass example code

--- lib/Lesson 6 Sorting/MaxProductOfThree/lib/max_product_of_three.rb
+++ lib/Lesson 6 Sorting/MaxProductOfThree/lib/max_product_of_three.rb
@@ -1,3 +1,13 @@
 def max_product_of_three(a)
+  maximal_product = a[0]*a[1]*a[2]
 
+  for i in 1..(a.length-3)
+    for j in (i+1)..(a.length-2)
+      for k in (j+1)..(a.length-1)
+        maximal_product = a[i]*a[j]*a[k] if maximal_product < a[i]*a[j]*a[k]
+      end
+    end
+  end
+
+  maximal_product
 end
