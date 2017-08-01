3cf5ba0cc20ce6692061dda9d13c62cffb07dd7c
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-01 14:51:42 +0100

    Add Example test

---
+++ lib/Lesson 6 Sorting/MaxProductOfThree/spec/max_product_of_three_spec.rb
@@ -0,0 +1,9 @@
+require 'max_product_of_three'
+
+describe 'Max Product of Three' do
+  describe 'Example Tests' do
+    it 'example: [-3 1, 2, -2, 5, 6] gives 60' do
+      expect(max_product_of_three([-3 1, 2, -2, 5, 6])).to eq 60
+    end
+  end
+end
