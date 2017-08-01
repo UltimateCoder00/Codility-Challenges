8116ea8e253ac194f7bd444a2529255444f2b643
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-01 15:21:42 +0100

    Add simple1 tests

--- lib/Lesson 6 Sorting/MaxProductOfThree/spec/max_product_of_three_spec.rb
+++ lib/Lesson 6 Sorting/MaxProductOfThree/spec/max_product_of_three_spec.rb
@@ -18,7 +18,10 @@ describe 'Max Product of Three' do
 
     context 'simple1' do
       it 'simple tests' do
-
+        expect(max_product_of_three([1, 2, 3, 4])).to eq 24
+        expect(max_product_of_three([2, 2, 2, 2, 2])).to eq 8
+        expect(max_product_of_three([4, 6, 2, 8, 10, 1])).to eq 480
+        expect(max_product_of_three([9, 8, 7, 6, 5, 4, 3])).to eq 504
       end
     end
 
