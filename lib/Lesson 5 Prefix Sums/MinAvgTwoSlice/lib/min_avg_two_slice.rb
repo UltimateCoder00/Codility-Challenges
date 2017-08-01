ab66e0460fbe5b79a5eef2a23d5cfea04c126555
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-01 16:50:02 +0100

    Refactor and pass medium_random performance test

--- lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/lib/min_avg_two_slice.rb
+++ lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/lib/min_avg_two_slice.rb
@@ -3,8 +3,12 @@ def min_avg_two_slice(a)
   array2 = []
 
   for i in 0...(a.length - 1)
+    sum = a[i]
+
     for j in (i+1)...(a.length)
-      array << (a[i..j].inject(0, :+) / (j - i + 1.0))
+      sum += a[j]
+
+      array << (sum / (j - i + 1.0))
       array2 << i
     end
   end
