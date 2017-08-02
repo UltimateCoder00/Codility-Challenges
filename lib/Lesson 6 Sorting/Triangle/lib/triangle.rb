91aec5ec6e5639fccd5b6c2610a7def8adb60e0d
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-02 14:45:40 +0100

    Add correctness test contexts

--- lib/Lesson 6 Sorting/Triangle/lib/triangle.rb
+++ lib/Lesson 6 Sorting/Triangle/lib/triangle.rb
@@ -1,3 +1,17 @@
 def triangle(a)
+  for i in 0...(a.length-2)
+    for j in (i+1)...(a.length-1)
+      for k in (j+1)...a.length
+        if a[i] + a[j] > a[k]
+          if a[j] + a[k] > a[i]
+            if a[k] + a[i] > a[j]
+              return 1
+            end
+          end
+        end
+      end
+    end
+  end
 
+  0
 end
--- lib/Lesson 6 Sorting/Triangle/spec/triangle_spec.rb
+++ lib/Lesson 6 Sorting/Triangle/spec/triangle_spec.rb
@@ -10,4 +10,66 @@ describe 'Triangle' do
       expect(triangle([10, 50, 5, 1])).to eq 0
     end
   end
+  
+  describe 'Correctness Tests' do
+    context 'extreme_empty' do
+      it 'empty sequence' do
+
+      end
+    end
+
+    context 'extreme_single' do
+      it '1-element sequence' do
+
+      end
+    end
+
+    context 'extreme_two_elems' do
+      it '2-element sequence' do
+
+      end
+    end
+
+    context 'extreme_negative1' do
+      it 'three equal negative numbers' do
+
+      end
+    end
+
+    context 'extreme_arith_overflow1' do
+      it 'overflow test, 3 MAXINTs' do
+
+      end
+    end
+
+    context 'extreme_arith_overflow2' do
+      it 'overflow test, 10 and 2 MININTs' do
+
+      end
+    end
+
+    context 'extreme_arith_overflow3' do
+      it 'overflow test, 0 and 2 MAXINTs' do
+
+      end
+    end
+
+    context 'medium1' do
+      it 'chaotic sequence of values from [0..100K], length=30' do
+
+      end
+    end
+
+    context 'medium2' do
+      it 'chaotic sequence of values from [0..1K], length=50' do
+
+      end
+    end
+
+    context 'medium3' do
+      it 'chaotic sequence of values from [0..1K], length=100' do
+
+      end
+    end
+  end
 end
