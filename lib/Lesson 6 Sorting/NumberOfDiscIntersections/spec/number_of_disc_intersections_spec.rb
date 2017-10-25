929881ea34c8758e2d0acfe6a92b952a1f97ca32
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-25 14:32:25 +0100

    Add [1, 10, 100, 1] gives 5 exmaple test

--- lib/Lesson 6 Sorting/NumberOfDiscIntersections/spec/number_of_disc_intersections_spec.rb
+++ lib/Lesson 6 Sorting/NumberOfDiscIntersections/spec/number_of_disc_intersections_spec.rb
@@ -5,5 +5,9 @@ describe 'Number Of Disc Intersections' do
     it 'example: [1, 5, 2, 1, 4, 0] gives 11' do
       expect(number_of_disc_intersections([1, 5, 2, 1, 4, 0])).to eq 11
     end
+
+    it 'example: [1, 10, 100, 1] gives 5' do
+      expect(number_of_disc_intersections([1, 10, 100, 1])).to eq 5
+    end
   end
 end
