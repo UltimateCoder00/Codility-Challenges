9326836aa2ff83c3ab77c18f27b34554509c7409
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-25 14:32:57 +0100

    Entend method and pass tests

--- lib/Lesson 6 Sorting/NumberOfDiscIntersections/lib/number_of_disc_intersections.rb
+++ lib/Lesson 6 Sorting/NumberOfDiscIntersections/lib/number_of_disc_intersections.rb
@@ -7,11 +7,19 @@ def number_of_disc_intersections(a)
     array1 << index - val
     array2 << index + val
   end
-  
+
   for j in 0...a.length-1
     for i in j+1...a.length
-      if (array1[j] >= array1[i]) || (array2[j] <= array2[i])
-          count += 1
+      if array1[j] <= array2[i]
+        if array2[j] >= array1[i]
+            count += 1
+        end
+      end
+
+      if array1[j] >= array2[i]
+        if array2[j] <= array1[i]
+            count += 1
+        end
       end
     end
   end
