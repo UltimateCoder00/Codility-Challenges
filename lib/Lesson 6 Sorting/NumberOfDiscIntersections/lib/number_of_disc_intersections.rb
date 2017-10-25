567d07aa8773c19cde7c1044166256742c989049
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-25 14:19:09 +0100

    Write method code and pass example test

--- lib/Lesson 6 Sorting/NumberOfDiscIntersections/lib/number_of_disc_intersections.rb
+++ lib/Lesson 6 Sorting/NumberOfDiscIntersections/lib/number_of_disc_intersections.rb
@@ -1,3 +1,20 @@
 def number_of_disc_intersections(a)
+  count = 0
+  array1 = []
+  array2 = []
+
+  a.each_with_index do |val, index|
+    array1 << index - val
+    array2 << index + val
+  end
   
+  for j in 0...a.length-1
+    for i in j+1...a.length
+      if (array1[j] >= array1[i]) || (array2[j] <= array2[i])
+          count += 1
+      end
+    end
+  end
+
+  count
 end
