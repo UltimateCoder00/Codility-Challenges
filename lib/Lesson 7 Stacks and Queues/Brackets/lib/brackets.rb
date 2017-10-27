548275c2d3fdf96cbcb93b92577f18b0bc8889cb
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-27 10:02:31 +0100

    Add brackets challenge method code

--- lib/Lesson 7 Stacks and Queues/Brackets/lib/brackets.rb
+++ lib/Lesson 7 Stacks and Queues/Brackets/lib/brackets.rb
@@ -1,3 +1,45 @@
 def brackets(s)
+  return 0 if s.length % 2 == 1
 
+  array = s.split("")
+
+  while array.count > 0
+    for i in 0...(array.length-1)
+      if array[i] == '('
+        if array[i+1] == ']'
+          return 0
+        elsif array[i+1] == '}'
+          return 0
+        elsif array[i+1] == ')'
+          array.delete_at(i+1)
+          array.delete_at(i)
+          next
+        end
+      elsif array[i] == '{'
+        if array[i+1] == ']'
+          return 0
+        elsif array[i+1] == '}'
+          array.delete_at(i+1)
+          array.delete_at(i)
+          next
+        elsif array[i+1] == ')'
+          return 0
+        end
+      elsif array[i] == '['
+        if array[i+1] == ']'
+          array.delete_at(i+1)
+          array.delete_at(i)
+          next
+        elsif array[i+1] == '}'
+          return 0
+        elsif array[i+1] == ')'
+          return 0
+        end
+      end
+    end
+
+    break
+  end
+
+  1
 end
