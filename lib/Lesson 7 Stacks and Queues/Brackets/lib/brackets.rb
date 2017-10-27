1a5bfbc4888a18b4757c884641255532d25dd1ec
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-27 15:00:38 +0100

    Add Brackets Challenge Code And Pass Tests

--- lib/Lesson 7 Stacks and Queues/Brackets/lib/brackets.rb
+++ lib/Lesson 7 Stacks and Queues/Brackets/lib/brackets.rb
@@ -2,8 +2,20 @@ def brackets(s)
   return 0 if s.length % 2 == 1
 
   array = s.split("")
+  return 0 unless array.count("(") == array.count(")")
+  return 0 unless array.count("{") == array.count("}")
+  return 0 unless array.count("[") == array.count("]")
+
+  return 0 if array[0] == ")"
+  return 0 if array[0] == "}"
+  return 0 if array[0] == "]"
+
+  return 0 if array[-1] == "("
+  return 0 if array[-1] == "{"
+  return 0 if array[-1] == "["
 
   while array.count > 0
+    p array
     for i in 0...(array.length-1)
       if array[i] == '('
         if array[i+1] == ']'
@@ -13,7 +25,7 @@ def brackets(s)
         elsif array[i+1] == ')'
           array.delete_at(i+1)
           array.delete_at(i)
-          next
+          break
         end
       elsif array[i] == '{'
         if array[i+1] == ']'
@@ -21,7 +33,7 @@ def brackets(s)
         elsif array[i+1] == '}'
           array.delete_at(i+1)
           array.delete_at(i)
-          next
+          break
         elsif array[i+1] == ')'
           return 0
         end
@@ -29,7 +41,7 @@ def brackets(s)
         if array[i+1] == ']'
           array.delete_at(i+1)
           array.delete_at(i)
-          next
+          break
         elsif array[i+1] == '}'
           return 0
         elsif array[i+1] == ')'
