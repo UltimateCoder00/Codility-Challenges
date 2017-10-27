f4e0eaf7ac2956585b0ac8b74dab69ed475c73eb
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-27 14:32:43 +0100

    Add Nesting Challenge Code And Pass Tests

--- lib/Lesson 7 Stacks and Queues/Nesting/lib/nesting.rb
+++ lib/Lesson 7 Stacks and Queues/Nesting/lib/nesting.rb
@@ -1,3 +1,28 @@
 def nesting(a)
+  return 0 if a[0] == ")"
+  return 0 if a[-1] == "("
+  return 0 if a.length % 2 == 1
 
+  array = a.split("")
+
+  count = array.count
+
+  while array.count > 0
+    count2 = array.count
+
+    for i in 0...array.length
+      if array[i] == "("
+        if array[i+1] == ")"
+          array.delete_at(i+1)
+          array.delete_at(i)
+          count -= 2
+          break
+        end
+      end
+    end
+
+    return 0 if count2 != count + 2
+  end
+
+  1
 end
