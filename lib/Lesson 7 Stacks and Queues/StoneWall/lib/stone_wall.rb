1da766c8d3d80c01b3fca600ba0df93077248cd7
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-28 17:59:47 +0100

    Add StoneWall Challenge Code Updates

--- lib/Lesson 7 Stacks and Queues/StoneWall/lib/stone_wall.rb
+++ lib/Lesson 7 Stacks and Queues/StoneWall/lib/stone_wall.rb
@@ -1,3 +1,19 @@
 def stone_wall(a)
+  p a
+  a.delete(0)
+  return 0 unless !!a.pop
 
+  count = 1
+  value = a[0]
+
+  for i in 0...(a.length-1)
+    if value > a[i]
+      stone_wall(a[0..(i-1)])
+    else
+      a[i] -= value
+      count += 1 if value > a[i+1]
+    end
+  end
+
+  count
 end
