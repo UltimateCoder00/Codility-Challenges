e53cff207b44815b6f01d695b141775fb31345ea
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-30 09:37:20 +0000

    Add Dominator Challenge Example Test

---
+++ spec/Lesson 8 Leader/Dominator/dominator_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 8 Leader/Dominator/dominator"
+
+describe 'Dominator' do
+
+  describe 'Example Tests' do
+    it 'Example: [3,4,3,2,3,-1,3,3] to 3' do
+      expect(dominator([3,4,3,2,3,-1,3,3])).to eq 3
+    end
+  end
+end
