2f5b2c14ef01d891f243c5c3f9f3ffab6da96a3d
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-10-31 13:39:49 +0000

    Add EquiLeader Challenge Code

--- lib/Lesson 8 Leader/EquiLeader/equi_leader.rb
+++ lib/Lesson 8 Leader/EquiLeader/equi_leader.rb
@@ -1,3 +1,29 @@
 def equi_leader(a)
+  count = 0
 
+  for i in 0...(a.length-1)
+    next if dominator(a[0..i]) == -1
+    next if dominator(a[i+1..-1]) == -1
+
+    if dominator(a[0..i]) == dominator(a[i+1..-1])
+      count += 1
+    end
+  end
+
+  count
+end
+
+def dominator(a)
+  return -1 if a.count < 1
+  b = a.uniq.sort
+  c = []
+
+  for i in 0...b.length
+    c << a.count(b[i])
+  end
+
+  return -1 if c.count(c.max) > 1
+  return -1 if a.count(b[c.index(c.max)]) <= a.length / 2
+
+  a[a.index(b[c.index(c.max)])]
 end
