4c4c24b0d9563901508b1bc56bcc72b6c62420be
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-01 11:06:53 +0000

    Add MaxDoubleSliceSum Challenge max_double_slice_sum(a) Method Code

--- lib/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum.rb
+++ lib/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum.rb
@@ -1,3 +1,17 @@
 def max_double_slice_sum(a)
+  max = 0
 
+  for i in 0...(a.length-2)
+    for j in (i+1)...(a.length-1)
+      for k in (j+1)...a.length
+        count = 0
+        count += a[(i+1)...j].inject(0){|sum,x| sum + x }
+        count += a[(j+1)...k].inject(0){|sum,x| sum + x }
+
+        max = count if max < count
+      end
+    end
+  end
+
+  max
 end
