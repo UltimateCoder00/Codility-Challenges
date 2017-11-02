afabe20cf3243a4cd00b3cc6d01993908977b93f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-02 09:21:17 +0000

    Add MaxProfit Challenge Example Test

---
+++ spec/Lesson 9 Maximum Slice Problem/MaxProfit/max_profit_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 9 Maximum Slice Problem/MaxProfit/max_profit"
+
+describe 'MaxProfit' do
+
+  describe 'Example Tests' do
+    it 'Example: [23171,21011,21123,21366,21013,21367] to 356' do
+      expect(equi_leader([23171,21011,21123,21366,21013,21367])).to eq 356
+    end
+  end
+end
