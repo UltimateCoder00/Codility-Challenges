5ebb73357d2c05fff90aae67286cb935e17ebacd
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-02 17:28:00 +0000

    Add MaxSliceSum Challenge Three_elements Test

--- spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
+++ spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
@@ -61,6 +61,116 @@ describe 'MaxSliceSum' do
       end
     end
 
+    context 'Three_elements' do
+      it '[-2,-2,-2] to -2' do
+        expect(max_slice_sum([-2,-2,-2])).to eq -2
+      end
+
+      it '[-2,-2,1] to 1' do
+        expect(max_slice_sum([-2,-2,1])).to eq 1
+      end
+
+      it '[-2,-2,3] to 3' do
+        expect(max_slice_sum([-2,-2,3])).to eq 3
+      end
+
+      it '[-2,1,-2] to 1' do
+        expect(max_slice_sum([-2,1,-2])).to eq 1
+      end
+
+      it '[-2,1,1] to 2' do
+        expect(max_slice_sum([-2,1,1])).to eq 2
+      end
+
+      it '[-2,1,3] to 4' do
+        expect(max_slice_sum([-2,1,3])).to eq 4
+      end
+
+      it '[-2,3,-2] to 3' do
+        expect(max_slice_sum([-2,3,-2])).to eq 3
+      end
+
+      it '[-2,3,1] to 4' do
+        expect(max_slice_sum([-2,3,1])).to eq 4
+      end
+
+      it '[-2,3,3] to 6' do
+        expect(max_slice_sum([-2,3,3])).to eq 6
+      end
+
+      it '[1,-2,-2] to 1' do
+        expect(max_slice_sum([1,-2,-2])).to eq 1
+      end
+
+      it '[1,-2,1] to 2' do
+        expect(max_slice_sum([1,-2,1])).to eq 1
+      end
+
+      it '[1,-2,3] to 1' do
+        expect(max_slice_sum([1,-2,3])).to eq 3
+      end
+
+      it '[1,1,-2] to 2' do
+        expect(max_slice_sum([1,1,-2])).to eq 2
+      end
+
+      it '[1,1,1] to 3' do
+        expect(max_slice_sum([1,1,1])).to eq 3
+      end
+
+      it '[1,1,3] to 5' do
+        expect(max_slice_sum([1,1,3])).to eq 5
+      end
+
+      it '[1,3,-2] to 4' do
+        expect(max_slice_sum([1,3,-2])).to eq 4
+      end
+
+      it '[1,3,1] to 5' do
+        expect(max_slice_sum([1,3,1])).to eq 5
+      end
+
+      it '[1,3,3] to 7' do
+        expect(max_slice_sum([1,3,3])).to eq 7
+      end
+
+      it '[3,-2,-2] to 3' do
+        expect(max_slice_sum([3,-2,-2])).to eq 3
+      end
+
+      it '[3,-2,1] to 3' do
+        expect(max_slice_sum([3,-2,1])).to eq 3
+      end
+
+      it '[3,-2,3] to 3' do
+        expect(max_slice_sum([3,-2,3])).to eq 4
+      end
+
+      it '[3,1,-2] to 4' do
+        expect(max_slice_sum([3,1,-2])).to eq 4
+      end
+
+      it '[3,1,1] to 5' do
+        expect(max_slice_sum([3,1,1])).to eq 5
+      end
+
+      it '[3,1,3] to 7' do
+        expect(max_slice_sum([3,1,3])).to eq 7
+      end
+
+      it '[3,3,-2] to 6' do
+        expect(max_slice_sum([3,3,-2])).to eq 6
+      end
+
+      it '[3,3,1] to 7' do
+        expect(max_slice_sum([3,3,1])).to eq 7
+      end
+
+      it '[3,3,3] to 9' do
+        expect(max_slice_sum([3,3,3])).to eq 9
+      end
+    end
+
     context 'Simple' do
       it '[1, 3, -5, 3, 7, 14, 29] to 53' do
         expect(max_slice_sum([1, 3, -5, 3, 7, 14, 29])).to eq 53
