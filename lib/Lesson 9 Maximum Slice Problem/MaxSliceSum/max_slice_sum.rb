f7514b5bca72bfc68576119e91e79e5818eb79ed
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-02 16:55:59 +0000

    Add MaxSliceSum Challenge max_slice_sum(a) Method Code

--- lib/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum.rb
+++ lib/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum.rb
@@ -1,3 +1,10 @@
 def max_slice_sum(a)
+  count = a[0]
+  for i in 0...a.length
+    for j in i...a.length
+      count = a[i..j].inject(0){|sum,x| sum + x } if count < a[i..j].inject(0){|sum,x| sum + x }
+    end
+  end
 
+  count
 end
