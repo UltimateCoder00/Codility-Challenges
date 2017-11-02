d8f2c87863558298f50b6c8b0ee09a2a9e2b4ad8
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-02 16:59:12 +0000

    Add MaxSliceSum Challenge One_element Test

--- spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
+++ spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
@@ -7,4 +7,20 @@ describe 'MaxSliceSum' do
       expect(max_slice_sum([3,2,-6,4,0])).to eq 5
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'One_element' do
+      it '[-10] to -10' do
+        expect(max_slice_sum([-10])).to eq -10
+      end
+
+      it '[0] to 0' do
+        expect(max_slice_sum([0])).to eq 0
+      end
+
+      it '[10] to 10' do
+        expect(max_slice_sum([10])).to eq 10
+      end
+    end
+  end
 end
