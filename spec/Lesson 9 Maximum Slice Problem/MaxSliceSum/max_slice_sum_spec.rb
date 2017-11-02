eaf3fb7d94d3257585eae6c705ce09ea57e27738
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-02 17:17:49 +0000

    Add MaxSliceSum Challenge Performance Test Templates

--- spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
+++ spec/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum_spec.rb
@@ -106,6 +106,18 @@ describe 'MaxSliceSum' do
     end
 
     describe 'Performance Tests' do
+      context 'High_low_1Kgarbage' do
+        it '[.....] to ...' do
+
+        end
+      end
+
+      context '1Kgarbage_high_low' do
+        it '[.....] to ...' do
+
+        end
+      end
+
       context 'Growing_saw' do
         it '[.....] to 252' do
           array = [0, 1, 3, 2, 1, 0, -1, 0, 1, 2, 4, 3, 2, 1, 0, -1, -2, 0, 1,
@@ -117,6 +129,18 @@ describe 'MaxSliceSum' do
                     -6, -7, 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 9, 8, 7, 6, 5, 4, 3,
                     2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8]
           expect(max_slice_sum(array)).to eq 252
+        end
+      end
+
+      context 'Blocks' do
+        it '[.....] to ...' do
+
+        end
+      end
+
+      context 'Growing_negative' do
+        it '[.....] to ...' do
+
         end
       end
     end
