ff256bf894e2842c0b4eedd6e736dd38acb314cc
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-03 10:22:25 +0000

    Add CountFactors Challenge Checkout If Prime Functionality

--- lib/Lesson 10 Prime And Composite Numbers/CountFactors/count_factors.rb
+++ lib/Lesson 10 Prime And Composite Numbers/CountFactors/count_factors.rb
@@ -1,7 +1,12 @@
-def count_factors(a)
-  count = 1
+require 'prime'
 
-  for i in 1..(a/2)
+def count_factors(a)
+  return 1 if a == 1
+  return 2 if Prime.prime?(a)
+  
+  count = 2
+
+  for i in 2..(a/2)
     count += 1 if a % i == 0
   end
 
