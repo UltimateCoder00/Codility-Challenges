dbe205957d438af93db3e49e58f3ce4e19d143c4
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-06 17:19:38 +0000

    Add MinPerimeterReactangle Challenge Example Test

---
+++ spec/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle"
+
+describe 'MinPerimeterRectangle' do
+
+  describe 'Example Tests' do
+    it 'Example: 30 to 22' do
+      expect(min_perimeter_rectangle(30)).to eq 22
+    end
+  end
+end
