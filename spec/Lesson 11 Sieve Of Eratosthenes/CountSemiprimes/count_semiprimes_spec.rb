c25712ef5334d1eb93be001508517480ada8396f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-07 16:58:32 +0000

    Fix CountSemiprimes Challenge Example Test

--- spec/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes_spec.rb
+++ spec/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes_spec.rb
@@ -3,8 +3,8 @@ require "./lib/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes"
 describe 'CountSemiprimes' do
 
   describe 'Example Tests' do
-    it 'Example: [1,4,16,26,10,20] to [10,4,0]' do
-      expect(count_semiprimes([1,4,16,26,10,20])).to eq [10,4,0]
+    it 'Example: (26, [1,4,16], [26,10,20]) to [10,4,0]' do
+      expect(count_semiprimes(26, [1,4,16], [26,10,20])).to eq [10,4,0]
     end
   end
 end
