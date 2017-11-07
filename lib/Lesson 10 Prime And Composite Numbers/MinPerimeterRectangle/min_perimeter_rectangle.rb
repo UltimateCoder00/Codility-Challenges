351ae7ccf764d79eb815176c85058591285b2e77
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-07 09:34:44 +0000

    Add MinPerimeterRectangle Challenge min_perimeter_rectangle(n) Method Code

--- lib/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle.rb
+++ lib/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle.rb
@@ -1,3 +1,13 @@
 def min_perimeter_rectangle(n)
+  count = 2 + 2*n
 
+  for i in 2...n
+    if n % i == 0
+      value = n / i
+
+      count = 2*i + 2*value if count > 2*i + 2*value
+    end
+  end
+
+  count
 end
