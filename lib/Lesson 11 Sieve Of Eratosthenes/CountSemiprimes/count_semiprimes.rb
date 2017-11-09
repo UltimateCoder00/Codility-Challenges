815cbb84ec23b81ef4c45e6c36801b8d94bdd1fc
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-09 19:28:16 +0000

    Add CountSemiprimes Challenge count_semiprimes(n,p,q) Method Code

--- lib/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes.rb
+++ lib/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes.rb
@@ -1,3 +1,19 @@
-def count_semiprimes(n, p, q)
+require 'prime'
 
+def count_semiprimes(n, p, q)
+  array = []
+
+  for i in 0...p.length
+    number = 0
+
+    for j in p[i]..q[i]
+      unless Prime.prime?(j)
+        
+      end
+    end
+
+    array << number
+  end
+
+  array
 end
