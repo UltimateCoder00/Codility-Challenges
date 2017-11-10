b74ef29fd5702198967c597841522e44f2ed9f0f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-10 12:18:34 +0000

    Add CountSemiprimes Challenge Extreme_four Test

--- spec/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes_spec.rb
+++ spec/Lesson 11 Sieve Of Eratosthenes/CountSemiprimes/count_semiprimes_spec.rb
@@ -17,5 +17,14 @@ describe 'CountSemiprimes' do
         expect(count_semiprimes(n, a, b)).to eq [0]
       end
     end
+
+    context 'Extreme_one' do
+      it 'Small N = 4' do
+        n = 4
+        a = [1, 2, 3, 4, 1, 2, 3, 1, 2, 1]
+        b = [4, 4, 4, 4, 3, 3, 3, 2, 2, 1]
+        expect(count_semiprimes(n, a, b)).to eq [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+      end
+    end
   end
 end
