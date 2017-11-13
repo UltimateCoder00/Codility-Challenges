e131bb4564c971c913abc7a9ec59355b8a93d296
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-13 17:15:41 +0000

    Add ChocolatesByNumbers Challenge chocolates_by_numbers(n, m) Method Code And Pass Example Test

--- lib/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers.rb
+++ lib/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers.rb
@@ -1,3 +1,20 @@
 def chocolates_by_numbers(n, m)
+  count = 0
 
+  array = [*0...10]
+  i = 0
+
+  loop do
+    count += 1
+    array[i] = nil
+    if i + m <= n
+      i = i + m
+    else
+      i = i + m - n
+    end
+
+    break if array[i] == nil
+  end
+
+  count
 end
