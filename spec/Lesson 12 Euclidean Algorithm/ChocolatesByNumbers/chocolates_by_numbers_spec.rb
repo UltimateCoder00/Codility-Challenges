622d7bee0ea229ee79c029b15a8ae7586dba2219
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-13 17:21:54 +0000

    Add ChocolatesByNumbers Challenge Simple Test

--- spec/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers_spec.rb
+++ spec/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers_spec.rb
@@ -10,16 +10,22 @@ describe 'ChocolatesByNumbers' do
 
   describe 'Correctness Tests' do
     context 'Extreme_small' do
-      it 'Very Small N And M: (1, 1) to 1' do
+      it 'Very Small N=1 And M=1 to 1' do
         expect(chocolates_by_numbers(1, 1)).to eq 1
       end
 
-      it 'Very Small N And M: (2, 1) to 2' do
+      it 'Very Small N=2 And M=1 to 2' do
         expect(chocolates_by_numbers(2, 1)).to eq 2
       end
 
-      it 'Very Small N And M: (1, 1) to 1' do
+      it 'Very Small N=1 And M=2 to 1' do
         expect(chocolates_by_numbers(1, 2)).to eq 1
+      end
+    end
+
+    context 'Simple' do
+      it 'N=24 And M=18 to 4' do
+        expect(chocolates_by_numbers(24, 18)).to eq 4
       end
     end
   end
