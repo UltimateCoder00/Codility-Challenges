8118e32799a8b6a890596c23b6ead149f54b0bdd
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-13 16:59:06 +0000

    Add ChocolatesByNumbers Challenge Example Test

---
+++ spec/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers"
+
+describe 'ChocolatesByNumbers' do
+
+  describe 'Example Tests' do
+    it 'Example: (10, 4) to 5' do
+      expect(chocolates_by_numbers(10, 4)).to eq 5
+    end
+  end
+end
