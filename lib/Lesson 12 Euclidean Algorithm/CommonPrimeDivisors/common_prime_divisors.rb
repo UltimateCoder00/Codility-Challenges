6f9d0c986a4af08f72212957ef5e2b27fc15a33f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-14 16:01:43 +0000

    Add CommonPrimeDivisors Challenge common_prime_divisors(a, b) Method Code And Pass Example Test

--- lib/Lesson 12 Euclidean Algorithm/CommonPrimeDivisors/common_prime_divisors.rb
+++ lib/Lesson 12 Euclidean Algorithm/CommonPrimeDivisors/common_prime_divisors.rb
@@ -1,3 +1,44 @@
-def common_prime_divisors(a, b)
+require 'prime'
 
+def common_prime_divisors(a, b)
+  count = 0
+
+  for i in 0...a.length
+    maxValue = [a[i],b[i]].max
+    minValue = [a[i],b[i]].min
+
+    if maxValue != minValue
+      next if Prime.prime?(maxValue)
+    end
+
+    breaking = 0
+
+    for j in (minValue+1)...maxValue
+      if maxValue % j == 0
+        if Prime.prime?(j)
+          breaking = 1
+        end
+      end
+    end
+
+    next if breaking == 1
+
+    for k in 2..minValue
+      if Prime.prime?(k)
+        if maxValue % k == 0
+          if minValue % k != 0
+            break
+          end
+        else
+          if minValue % k == 0
+            break
+          end
+        end
+      end
+
+      count += 1 if k == minValue
+    end
+  end
+
+  count
 end
