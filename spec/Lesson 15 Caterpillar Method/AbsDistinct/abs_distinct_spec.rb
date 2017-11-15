1dbca70fd1eed0a3fad728c49c06bbe53ddad347
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-15 16:31:40 +0000

    Add AbsDistinct Challenge One_element Test

--- spec/Lesson 15 Caterpillar Method/AbsDistinct/abs_distinct_spec.rb
+++ spec/Lesson 15 Caterpillar Method/AbsDistinct/abs_distinct_spec.rb
@@ -7,4 +7,20 @@ describe 'abs_distinct' do
       expect(abs_distinct([-5,-3,-1,0,3,6])).to eq 5
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'One_element' do
+      it '[-10] to 1' do
+        expect(abs_distinct([-10])).to eq 1
+      end
+
+      it '[0] to 1' do
+        expect(abs_distinct([0])).to eq 1
+      end
+
+      it '[10] to 1' do
+        expect(abs_distinct([10])).to eq 1
+      end
+    end
+  end
 end
