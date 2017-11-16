9fd8b59b1e88c554271f906925977a6fdeb694df
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-16 16:25:06 +0000

    Add CountDistinctSlices Challenge Single Test

--- spec/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices_spec.rb
+++ spec/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices_spec.rb
@@ -2,9 +2,25 @@ require "./lib/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_s
 
 describe 'CountDistinctSlices' do
 
-  describe 'Example Tests' do
-    it 'Example: (6, [3,4,5,5,2]) to 9' do
+  describe 'Example Test' do
+    it 'Example' do
       expect(count_distinct_slices(6,[3,4,5,5,2])).to eq 9
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Single' do
+      it 'Simple Element 1' do
+        expect(count_distinct_slices(100000,[100000])).to eq 1
+      end
+
+      it 'Simple Element 2' do
+        expect(count_distinct_slices(100000,[0])).to eq 1
+      end
+
+      it 'Simple Element 3' do
+        expect(count_distinct_slices(0,[0])).to eq 1
+      end
     end
   end
 end
