3c4e864773f078ae14b0646aa41bb6a1ecca7909
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-16 16:19:24 +0000

    Add CountDistinctSlices Challenge count_distinct_slices(m, a) Method Code

--- lib/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices.rb
+++ lib/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices.rb
@@ -1,3 +1,15 @@
 def count_distinct_slices(m, a)
+  count = 0
 
+  for i in 0...a.length
+    for j in i...a.length
+      if a[i..j].uniq == a[i..j]
+        count += 1
+      else
+        break
+      end
+    end
+  end
+
+  count < 1000000000 ? count : 1000000000
 end
