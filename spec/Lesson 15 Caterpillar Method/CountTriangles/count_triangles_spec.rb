57e47389f1af401229b275b48bb3913c80b0a218
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-17 17:07:10 +0000

    Add CountTriangles Challenge Extreme_empty Test

--- spec/Lesson 15 Caterpillar Method/CountTriangles/count_triangles_spec.rb
+++ spec/Lesson 15 Caterpillar Method/CountTriangles/count_triangles_spec.rb
@@ -3,8 +3,20 @@ require "./lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles"
 describe 'CountTriangles' do
 
   describe 'Example Test' do
-    it 'Example' do
+    it 'Example, Positive Answer, Length=6' do
       expect(count_triangles([10,2,5,1,8,12])).to eq 4
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Extreme_empty' do
+      it 'Empty Sequence' do
+        expect(count_triangles([])).to eq 0
+      end
+
+      it '[5,3,3]' do
+        expect(count_triangles([5, 3, 3])).to eq 1
+      end
     end
   end
 end
