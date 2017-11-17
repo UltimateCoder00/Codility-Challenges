eccb4e974b497e2f9b26b8dcc9e26a03a2fd32a5
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-17 16:55:33 +0000

    Add CountTriangles Challenge count_triangles(a) Method Code

--- lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles.rb
+++ lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles.rb
@@ -1,3 +1,19 @@
 def count_triangles(a)
+  count = 0
 
+  for i in 0...(a.length-2)
+    for j in (i+1)...(a.length-1)
+      for k in (j+1)...(a.length)
+        if a[i] + a[j] > a[k]
+          if a[i] + a[k] > a[j]
+            if a[j] + a[k] > a[i]
+              count += 1
+            end
+          end
+        end
+      end
+    end
+  end
+
+  count
 end
