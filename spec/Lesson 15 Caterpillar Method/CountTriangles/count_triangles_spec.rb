c3f8979eecc3b9ab21e34416fb11258df9a1710d
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-17 16:49:34 +0000

    Add CountTriangles Challenge Example Test

---
+++ spec/Lesson 15 Caterpillar Method/CountTriangles/count_triangles_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles"
+
+describe 'CountTriangles' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(count_triangles([10,2,5,1,8,12])).to eq 4
+    end
+  end
+end
