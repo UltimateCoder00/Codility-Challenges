c7c6189e0f99fc1eebd426db2d01d2e789440930
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-20 13:55:34 +0000

    Fix MinAbsSumOfTwo Challenge min_abs_sum_of_two(a) Method Code

--- lib/Lesson 15 Caterpillar Method/MinAbsSumOfTwo/min_abs_sum_of_two.rb
+++ lib/Lesson 15 Caterpillar Method/MinAbsSumOfTwo/min_abs_sum_of_two.rb
@@ -1,7 +1,7 @@
 def min_abs_sum_of_two(a)
   min_abs = (a[0]+a[0]).abs
 
-  for i in 0...(a.length-1)
+  for i in 0...a.length
     for j in i...a.length
       min_abs = (a[i]+a[j]).abs if min_abs > (a[i]+a[j]).abs
     end
