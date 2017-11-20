b9b94cc4a07dc91f49c214416461f7c329e0bd4c
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-20 13:46:45 +0000

    Add MinAbsSumOfTwo Challenge Extreme_single Tests

--- spec/Lesson 15 Caterpillar Method/MinAbsSumOfTwo/min_abs_sum_of_two_spec.rb
+++ spec/Lesson 15 Caterpillar Method/MinAbsSumOfTwo/min_abs_sum_of_two_spec.rb
@@ -3,12 +3,26 @@ require "./lib/Lesson 15 Caterpillar Method/MinAbsSumOfTwo/min_abs_sum_of_two"
 describe 'MinAbsSumOfTwo' do
 
   describe 'Example Tests' do
-    it 'Example: [1,4,-3]' do
+    it 'Example 1' do
       expect(min_abs_sum_of_two([1,4,-3])).to eq 1
     end
 
-    it 'Example: [-8,4,5,-10,3]' do
+    it 'Example 2' do
       expect(min_abs_sum_of_two([-8,4,5,-10,3])).to eq 3
+    end
+  end
+
+  describe 'Correctness Tests' do
+    it 'Extreme_single' do
+      expect(min_abs_sum_of_two([-1000000000])).to eq 2000000000
+    end
+
+    it 'Extreme_single' do
+      expect(min_abs_sum_of_two([0])).to eq 0
+    end
+
+    it 'Extreme_single' do
+      expect(min_abs_sum_of_two([1000000000])).to eq 2000000000
     end
   end
 end
