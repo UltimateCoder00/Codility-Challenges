bc9020c5fd6e08db4cc964437fe70823db61b638
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-21 15:09:26 +0000

    Fix TieRopes Challenge Example Test

--- spec/Lesson 16 Greedy Algorithms/TieRopes/tie_ropes_spec.rb
+++ spec/Lesson 16 Greedy Algorithms/TieRopes/tie_ropes_spec.rb
@@ -2,9 +2,9 @@ require "./lib/Lesson 16 Greedy Algorithms/TieRopes/tie_ropes"
 
 describe 'TieRopes' do
 
-  describe 'Example Tests' do
+  describe 'Example Test' do
     it 'Example' do
-      expect(tie_ropes([1,2,3,4,1,1,3])).to eq 4
+      expect(tie_ropes(4, [1,2,3,4,1,1,3])).to eq 4
     end
   end
 end
