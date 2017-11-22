3b98aa1854ee2b849f5b4f226e7bda53f3aa7260
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-22 15:01:49 +0000

    Add TennisTournament Challenge Example Tests

---
+++ spec/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/TennisTournament/tennis_tournament_spec.rb
@@ -0,0 +1,14 @@
+require "./lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/TennisTournament/tennis_tournament"
+
+describe 'TennisTournament' do
+
+  describe 'Example Test' do
+    it 'Example 1' do
+      expect(tennis_tournament(5, 3)).to eq 2
+    end
+
+    it 'Example 2' do
+      expect(tennis_tournament(10, 3)).to eq 3
+    end
+  end
+end
