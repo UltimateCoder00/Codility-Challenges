75a3be8da5867abd149d67bded85694b9d536384
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-22 15:08:02 +0000

    Add TennisTournament Challenge Only_two_players Tests

--- spec/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/TennisTournament/tennis_tournament_spec.rb
+++ spec/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/TennisTournament/tennis_tournament_spec.rb
@@ -11,4 +11,20 @@ describe 'TennisTournament' do
       expect(tennis_tournament(10, 3)).to eq 3
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'Only_two_players' do
+      it 'Only_two_players 1' do
+        expect(tennis_tournament(2, 1)).to eq 1
+      end
+
+      it 'Only_two_players 2' do
+        expect(tennis_tournament(2, 9)).to eq 1
+      end
+
+      it 'Only_two_players 3' do
+        expect(tennis_tournament(2, 30000)).to eq 1
+      end
+    end
+  end
 end
