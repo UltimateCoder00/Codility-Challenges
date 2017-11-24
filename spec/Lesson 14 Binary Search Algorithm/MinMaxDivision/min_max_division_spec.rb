94dc75e2b64175399a9edb44feeafa41fa941a3e
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-24 14:17:12 +0000

    Add MinMaxDivision Challenge Example Test

---
+++ spec/Lesson 14 Binary Search Algorithm/MinMaxDivision/min_max_division_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 14 Binary Search Algorithm/MinMaxDivision/min_max_division"
+
+describe 'MinMaxDivision' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(min_max_division(3, 5, [2, 1, 5, 1, 2, 2, 2])).to eq 6
+    end
+  end
+end
