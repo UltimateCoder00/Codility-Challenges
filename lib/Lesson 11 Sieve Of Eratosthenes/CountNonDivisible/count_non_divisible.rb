5e57784b18bf45ecebc85fa1c1dab822e4ae43b2
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-27 14:42:31 +0000

    Add CountNonDivisible Challenge count_non_divisible(a) Method Code

--- lib/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible.rb
+++ lib/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible.rb
@@ -1,3 +1,15 @@
 def count_non_divisible(a)
+  array = []
 
+  for i in 0...a.length
+    count = 0
+
+    for j in 0...a.length
+      count += 1 if a[i] % a[j] != 0
+    end
+
+    array << count
+  end
+
+  array
 end
