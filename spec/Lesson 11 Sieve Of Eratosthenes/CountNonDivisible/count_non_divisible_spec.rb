b7063d9b8234881de214bfd4f05f2ef6d32a010e
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-27 14:48:02 +0000

    Add CountNonDivisible Challenge Extreme_simple Tests

--- spec/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible_spec.rb
+++ spec/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible_spec.rb
@@ -7,4 +7,16 @@ describe 'CountNonDivisible' do
       expect(count_non_divisible([3,1,2,3,6])).to eq [2,4,3,2,0]
     end
   end
+
+  describe 'Correctness Test' do
+    context 'Extreme_simple' do
+      it 'Extreme Simple 1' do
+        expect(count_non_divisible([2])).to eq [0]
+      end
+
+      it 'Extreme Simple 2' do
+        expect(count_non_divisible([1]*9)).to eq [0]*9
+      end
+    end
+  end
 end
