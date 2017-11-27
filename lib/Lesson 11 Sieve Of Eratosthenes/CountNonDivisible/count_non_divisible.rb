2d35bb07b718bf079f0940a9d41ab887a13b2ccf
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-27 15:24:47 +0000

    Refactor CountNonDivisible Challenge count_non_divisible(a) Method Code

--- lib/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible.rb
+++ lib/Lesson 11 Sieve Of Eratosthenes/CountNonDivisible/count_non_divisible.rb
@@ -1,11 +1,13 @@
 def count_non_divisible(a)
+  feq_hash = a.inject(Hash.new(0)) {|hash,word| hash[word] += 1; hash }
+
   array = []
 
   for i in 0...a.length
     count = 0
 
-    for j in 0...a.length
-      count += 1 if a[i] % a[j] != 0
+    feq_hash.each do |x|
+      count += x[1] if a[i] % x[0] != 0
     end
 
     array << count
