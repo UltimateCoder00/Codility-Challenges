e5aa791649ff7db5c4f55d27919a23810efc774e
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-28 16:59:46 +0000

    Add Brackets Challenge Negative_match Tests

--- spec/Lesson 7 Stacks And Queues/Brackets/brackets_spec.rb
+++ spec/Lesson 7 Stacks And Queues/Brackets/brackets_spec.rb
@@ -2,12 +2,36 @@ require "./lib/Lesson 7 Stacks And Queues/Brackets/brackets"
 
 describe 'Brackets' do
   describe 'Example Tests' do
-    it 'Example 1' do
+    it 'Example1' do
       expect(brackets("{[()()]}")).to eq 1
     end
 
-    it 'Example 2' do
+    it 'Example2' do
       expect(brackets("([)()]")).to eq 0
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Negative_match' do
+      it 'Invalid Structures 1' do
+        expect(brackets("))((")).to eq 0
+      end
+
+      it 'Invalid Structures 2' do
+        expect(brackets("())(()")).to eq 0
+      end
+
+      it 'Invalid Structures 3' do
+        expect(brackets("[()}")).to eq 0
+      end
+
+      it 'Invalid Structures 4' do
+        expect(brackets("{([][][])]")).to eq 0
+      end
+
+      it 'Invalid Structures 5' do
+        expect(brackets("{{{{")).to eq 0
+      end
     end
   end
 end
