90a799124567d8cbdf456b3f3d2566a7a70a6fdd
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-29 16:18:36 +0000

    Add Fish Challenge Example Test

---
+++ spec/Lesson 7 Stacks And Queues/Fish/fish_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/Lesson 7 Stacks And Queues/Fish/fish"
+
+describe 'Fish' do
+  describe 'Example Test' do
+    it 'Example' do
+      expect(fish([4,3,2,1,5],[0,1,0,0,0])).to eq 2
+    end
+  end
+end
