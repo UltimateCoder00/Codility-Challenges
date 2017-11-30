bbbfbb1d684f6340ae8608ce8882d7fcd1fc2c47
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-11-30 17:03:15 +0000

    Add Nesting Challenge nesting(s) Method Code

--- lib/Lesson 7 Stacks And Queues/Nesting/nesting.rb
+++ lib/Lesson 7 Stacks And Queues/Nesting/nesting.rb
@@ -1,3 +1,26 @@
 def nesting(s)
+  return 1 if s.length == 0
+  return 0 if s.length % 2 == 1
+  return 0 if s[0] == ')'
+  return 0 if s[-1] == '('
 
+  brackets_array = s.chars
+
+  test_array = [brackets_array[0]]
+  count = 1
+
+  for i in 1...brackets_array.length
+    if brackets_array[i] == '('
+      test_array << brackets_array[i]
+      count += 1
+      next
+    else
+      return 0 if test_array.count == 0
+
+      test_array.pop
+      count -= 1
+    end
+  end
+
+  1
 end
