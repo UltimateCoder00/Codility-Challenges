0feebab596c72e6baba625cdc9946af10c3f6ceb
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-01 23:18:58 +0000

    Add StoneWall Challenge Example Test

---
+++ spec/Lesson 7 Stacks And Queues/StoneWall/stone_wall_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/Lesson 7 Stacks And Queues/StoneWall/stone_wall"
+
+describe 'StoneWall' do
+  describe 'Example Tests' do
+    it 'Example' do
+      expect(stone_wall([8,8,5,7,9,8,7,4,8])).to eq 7
+    end
+  end
+end
