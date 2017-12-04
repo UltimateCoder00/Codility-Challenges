60795145cd908df5a0ce9ef7e3fa503e63181252
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-04 15:09:51 +0000

    Add StoneWall Challenge Simple1 Test

--- spec/Lesson 7 Stacks And Queues/StoneWall/stone_wall_spec.rb
+++ spec/Lesson 7 Stacks And Queues/StoneWall/stone_wall_spec.rb
@@ -6,4 +6,12 @@ describe 'StoneWall' do
       expect(stone_wall([8,8,5,7,9,8,7,4,8])).to eq 7
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'Simple1' do
+      it 'Example' do
+        expect(stone_wall([1,2,3,3,2,1])).to eq 3
+      end
+    end
+  end
 end
