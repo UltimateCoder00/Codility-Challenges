bf94add61f8fe85f2b9f038be60ab93cfcc51b9b
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-04 15:07:32 +0000

    Add StoneWall Challenge stone_wall(h) Method Code

--- lib/Lesson 7 Stacks And Queues/StoneWall/stone_wall.rb
+++ lib/Lesson 7 Stacks And Queues/StoneWall/stone_wall.rb
@@ -1,3 +1,28 @@
 def stone_wall(h)
+  return 1 if h.length == 1
+  return 1 if h.uniq.length == 1
 
+  count = 0
+
+  while true
+    break if h.count == 0
+    if h[0] == 0
+      h.shift
+      next
+    end
+
+    value = h[0]
+    count += 1
+    h.shift
+
+    for i in 0...h.length
+      if h[i] >= value
+        h[i] -= value
+      else
+        break
+      end
+    end
+  end
+
+  count
 end
