44f18d97a0db322f2d6a4946d340ccb1ef249501
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-05 17:04:38 +0000

    Add Fish Challenge def fish(a, b) Method Code

--- lib/Lesson 7 Stacks And Queues/Fish/fish.rb
+++ lib/Lesson 7 Stacks And Queues/Fish/fish.rb
@@ -1,4 +1,31 @@
 def fish(a, b)
+  return b.count if b.uniq.count == 1
+
+  extra_counting = 0
+  index = 0
+
+  for i in 0...b.length
+    if b[i] == 0
+      extra_counting += 1
+    else
+      index = i
+      break
+    end
+  end
+
+  a = a[i..-1]
+  b = b[i..-1]
+
+  (b.length-1).downto(0) do |i|
+    if b[i] == 1
+      a.pop
+      b.pop
+      extra_counting += 1
+    else
+      break
+    end
+  end
+
   while true
     count = a.count
 
@@ -21,5 +48,5 @@ def fish(a, b)
     break if a.count == count
   end
 
-  a.count
+  a.count + extra_counting
 end
