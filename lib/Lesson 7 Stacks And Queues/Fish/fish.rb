1b0d44580e45f4acadeb85039a60a015987b924f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-05 16:23:18 +0000

    Add Fish Challenge Extreme_small Tests

--- lib/Lesson 7 Stacks And Queues/Fish/fish.rb
+++ lib/Lesson 7 Stacks And Queues/Fish/fish.rb
@@ -1,3 +1,25 @@
 def fish(a, b)
+  while true
+    count = a.count
 
+    for i in 0...a.length
+      if b[i] == 1
+        if b[i+1] == 0
+          if a[i] > a[i+1]
+            a.delete_at(i+1)
+            b.delete_at(i+1)
+          elsif a[i] < a[i+1]
+            a.delete_at(i)
+            b.delete_at(i)
+          else
+            next
+          end
+        end
+      end
+    end
+
+    break if a.count == count
+  end
+
+  a.count
 end
--- spec/Lesson 7 Stacks And Queues/Fish/fish_spec.rb
+++ spec/Lesson 7 Stacks And Queues/Fish/fish_spec.rb
@@ -3,7 +3,35 @@ require "./lib/Lesson 7 Stacks And Queues/Fish/fish"
 describe 'Fish' do
   describe 'Example Test' do
     it 'Example' do
-      expect(fish([4,3,2,1,5],[0,1,0,0,0])).to eq 2
+      expect(fish([4,3,2,1,5], [0,1,0,0,0])).to eq 2
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Extreme_small' do
+      it '1 Fish 1' do
+        expect(fish([1], [0])).to eq 1
+      end
+
+      it '1 Fish 2' do
+        expect(fish([1000000000], [1])).to eq 1
+      end
+
+      it '2 Fishes 1' do
+        expect(fish([1000000000,999999999], [0,0])).to eq 2
+      end
+
+      it '2 Fishes 2' do
+        expect(fish([1000000000,999999999], [1,0])).to eq 1
+      end
+
+      it '2 Fishes 3' do
+        expect(fish([1000000000,999999999], [0,1])).to eq 2
+      end
+
+      it '2 Fishes 4' do
+        expect(fish([999999999,1000000000], [1,0])).to eq 1
+      end
     end
   end
 end
