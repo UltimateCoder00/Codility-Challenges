7143bdebe3d3a658fe228ce25455ac321b18ba1b
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-08 14:08:39 +0000

    Add LongestPassword Challenge longest_password(s) Method COde

--- lib/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password.rb
+++ lib/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password.rb
@@ -1,3 +1,14 @@
 def longest_password(s)
+  s.downcase.split(" ").uniq.sort_by(&:length).reverse.each do |pass|
+    return pass.length if password_correct?(pass)
+  end
+  -1
+end
 
+def password_correct?(pass)
+  num_length = pass.delete("^0-9").length
+  str_length = pass.delete("^a-z").length
+  return false unless num_length % 2 == 1
+  return false unless str_length % 2 == 0
+  num_length + str_length == pass.length ? true : false
 end
