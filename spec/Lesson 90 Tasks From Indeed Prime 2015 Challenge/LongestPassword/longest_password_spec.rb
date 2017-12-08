166dd6aa56f88f7cf4870b55f995d1e6a5ea037f
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-08 13:34:40 +0000

    Add LongestPassword Challenge Example Test

---
+++ spec/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password"
+
+describe 'LongestPassword' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(longest_password("test 5 a0A pass007 ?xy1")).to eq 7
+    end
+  end
+end
