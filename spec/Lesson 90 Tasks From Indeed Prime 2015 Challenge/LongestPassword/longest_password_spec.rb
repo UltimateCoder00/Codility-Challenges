fb903dbb499afab5c1d7c919cf0ab4636d370b6b
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-08 14:20:52 +0000

    Add LongestPassword Challenge Simple Tests

--- spec/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password_spec.rb
+++ spec/Lesson 90 Tasks From Indeed Prime 2015 Challenge/LongestPassword/longest_password_spec.rb
@@ -7,4 +7,24 @@ describe 'LongestPassword' do
       expect(longest_password("test 5 a0A pass007 ?xy1")).to eq 7
     end
   end
+
+  describe 'Correctness Tests' do
+    context 'Simple' do
+      it 'Short And Simple Tests 1' do
+        expect(longest_password("asdf qwerty1 ?!a a vv 91")).to eq 7
+      end
+
+      it 'Short And Simple Tests 2' do
+        expect(longest_password("123 ??123asd 5ZAQW 3")).to eq 5
+      end
+
+      it 'Short And Simple Tests 3' do
+        expect(longest_password("asdf! 3ab qqqq adw3")).to eq 3
+      end
+
+      it 'Short And Simple Tests 4' do
+        expect(longest_password("'v?# rb587 :{1bQ!&)J TD;Wa $Os")).to eq 5
+      end
+    end
+  end
 end
