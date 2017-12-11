0a3b226bd19a315c931e7bf57cb72f0f985c95b7
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-11 16:56:29 +0000

    Add StrSymmetryPoint Challenge str_symmetry_point(s) Method Code And Pass Example Tests

--- lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
+++ lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
@@ -1,3 +1,29 @@
 def str_symmetry_point(s)
+  return 0 if s.length == 1
+  return -1 if s.length == 0
 
+  array = s.chars
+  last = array.length - 1
+  middle = -1
+
+  if s.length % 2 == 1
+    middle = array.length / 2
+  end
+
+  count = 0
+
+  for i in 0...s.length
+    count = i
+
+    break if i == middle
+
+    if array[i] == array[last]
+      last -= 1
+      next
+    else
+      break
+    end
+  end
+
+  count
 end
