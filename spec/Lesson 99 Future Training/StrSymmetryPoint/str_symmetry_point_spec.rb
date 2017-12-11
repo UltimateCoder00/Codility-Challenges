b98205dd07054fdeda5949131481d3dc28dd144c
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-11 16:58:29 +0000

    Add StrSymmetryPoint Challenge Extreme_empty_or_one Tests

--- spec/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point_spec.rb
+++ spec/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point_spec.rb
@@ -2,13 +2,25 @@ require "./lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point"
 
 describe 'StrSymmetryPoint' do
 
-  describe 'Example Test' do
+  describe 'Example Tests' do
     it 'Example 1' do
       expect(str_symmetry_point("racecar")).to eq 3
     end
 
     it 'Example 2' do
       expect(str_symmetry_point("x")).to eq 0
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Extreme_empty_or_one' do
+      it 'Empty' do
+        expect(str_symmetry_point("")).to eq -1
+      end
+
+      it 'One Character Strings' do
+        expect(str_symmetry_point("a")).to eq 0
+      end
     end
   end
 end
