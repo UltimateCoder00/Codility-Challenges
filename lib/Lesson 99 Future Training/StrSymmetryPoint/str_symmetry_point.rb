969384563d9a21d40019ec024b0de177ada51f9a
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-12 16:45:04 +0000

    Refactor StrSymmetryPoint Chalenge str_symmetry_point(s) Method Code

--- lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
+++ lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
@@ -2,36 +2,9 @@ def str_symmetry_point(s)
   return 0 if s.length == 1
   return -1 if s.length == 0
 
-  array = s.chars
-  last = array.length - 1
-  middle = -1
-  no_middle = -1
-
-  if s.length % 2 == 1
-    middle = array.length / 2
-  else
-    no_middle = array.length / 2
+  for i in 0..(s.length / 2)
+    return -1 if s[i] != s[s.length - 1 - i]
   end
 
-  count = 0
-
-  for i in 0...s.length
-    count = i
-
-    break if i == middle
-
-    if array[i] == array[last]
-      if no_middle != -1
-        return -1 if no_middle == i
-      end
-
-      last -= 1
-      next
-    else
-      return -1 if i < array.length / 2
-      break
-    end
-  end
-
-  count
+  s.length % 2 == 0 ? -1 : s.length / 2
 end
