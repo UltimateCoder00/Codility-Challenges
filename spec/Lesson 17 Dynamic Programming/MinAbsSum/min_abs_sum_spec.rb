3f8875f0fae68baf9814159d4e04d9386d05963a
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-13 10:30:13 +0000

    Add MinAbsSum Challenge Example Test

---
+++ spec/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum"
+
+describe 'MinAbsSum' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(min_abs_sum([1,5,2,-1])).to eq 0
+    end
+  end
+end
