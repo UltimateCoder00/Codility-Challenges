c4c6ae3bc8433f89f0ffc3e0c7c806eea5694ef4
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-18 14:55:31 +0000

    Add SocksLaundering Challenge socks_laundering(k, c, d) Method Code & Pass Example Test

--- lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
+++ lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
@@ -1,3 +1,29 @@
 def socks_laundering(k, c, d)
+  c.sort!
+  d.sort!
+  count = 0
 
+  (c.length - 1).downto(1) do |i|
+    if c[i] == c[i-1]
+      c.delete_at(i)
+      c.delete_at(i-1)
+      count += 1
+    end
+  end
+
+  e = d.dup
+
+  (e.length - 1).downto(1) do |i|
+    if e[i] == e[i-1]
+      e.delete_at(i)
+      e.delete_at(i-1)
+    end
+  end
+
+  f = e - c
+  g = e - f
+
+  return count + k if g.length >= k
+  h = d - f - g
+  count
 end
