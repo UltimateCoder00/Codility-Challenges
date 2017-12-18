f59e54ebf1e807fe967c57efb5da5c592b4f8a1c
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-18 14:20:04 +0000

    Add SocksLaundering Challenge Example Test

---
+++ spec/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering"
+
+describe 'SocksLaundering' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(socks_laundering(2, [1,2,1,1], [1,4,3,2,4])).to eq 3
+    end
+  end
+end
