eae7606535e5912069e2349b48ef141e93f4cede
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-19 16:04:25 +0000

    Fix SocksLaundering Challenge socks_laundering(k, c, d) Method & Pass All Tests

--- lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
+++ lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
@@ -1,5 +1,6 @@
 def socks_laundering(k, c, d)
   count = 0
+  count2 = 0
 
   if k >= d.length
     array = (c + d).sort
@@ -30,8 +31,8 @@ def socks_laundering(k, c, d)
 
   (e.length - 1).downto(1) do |i|
     if e[i] == e[i-1]
-      e.delete_at(i)
-      e.delete_at(i-1)
+      e.delete_at(i) if c.include?(e[i])
+      count2 += 1
     end
   end
 
@@ -39,7 +40,9 @@ def socks_laundering(k, c, d)
   g = e - f
 
   return count + k if g.length >= k
-  h = d - f - g
 
-  count
+  count += g.length
+  k -= g.length
+
+  k == g.length ? count : count + (k/2)
 end
