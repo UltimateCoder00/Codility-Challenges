0693d1f07572a571e1c5e3d709dc2506aa9144d2
ultimatecoder00 <hasansonmez100@hotmail.com>
2017-12-19 16:40:27 +0000

    Refactor SocksLaundering Challenge socks_laundering(k, c, d) Method

--- lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
+++ lib/Lesson 92 Tasks From Indeed Prime 2016 College Coders Challenge/SocksLaundering/socks_laundering.rb
@@ -1,48 +1,31 @@
 def socks_laundering(k, c, d)
-  count = 0
-  count2 = 0
-
   if k >= d.length
     array = (c + d).sort
-
-    (array.length - 1).downto(1) do |i|
-      if array[i] == array[i-1]
-        array.delete_at(i)
-        array.delete_at(i-1)
-        count += 1
-      end
-    end
-
-    return count
+    return count_array_duplicates(array)
   end
 
-  c.sort!
-  d.sort!
+  return count_array_duplicates(c) + k if (c & d.uniq).length >= k
 
-  (c.length - 1).downto(1) do |i|
-    if c[i] == c[i-1]
-      c.delete_at(i)
-      c.delete_at(i-1)
+  count = count_array_duplicates(c) 
+  dirty_array = c & d.uniq
+
+  count += dirty_array.length
+  k -= dirty_array.length
+
+  k == dirty_array.length ? count : count + (k/2)
+end
+
+def count_array_duplicates(array)
+  array.sort!
+  count = 0
+
+  (array.length - 1).downto(1) do |i|
+    if array[i] == array[i-1]
+      array.delete_at(i)
+      array.delete_at(i-1)
       count += 1
     end
   end
 
-  e = d.dup
-
-  (e.length - 1).downto(1) do |i|
-    if e[i] == e[i-1]
-      e.delete_at(i) if c.include?(e[i])
-      count2 += 1
-    end
-  end
-
-  f = e - c
-  g = e - f
-
-  return count + k if g.length >= k
-
-  count += g.length
-  k -= g.length
-
-  k == g.length ? count : count + (k/2)
+  count
 end
