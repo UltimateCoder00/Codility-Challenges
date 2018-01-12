270c700c36f069cb89cff2113f290899135fe195
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-12 16:06:02 +0000

    Add Flags Challenge Example Test

---
+++ spec/Lesson 10 Prime And Composite Numbers/Flags/flags_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 10 Prime And Composite Numbers/Flags/flags"
+
+describe 'Flags' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(flags([1,5,3,4,3,4,1,2,3,4,6,2])).to eq 3
+    end
+  end
+end
