7569b719a1d97eb120deac2a1ca86fb39dd1f8c1
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-16 16:15:35 +0000

    Add RectangleBuilderGreaterArea Challenge rectangle_builder_greater_area(a, x) Method Code

--- lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
+++ lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
@@ -1,3 +1,15 @@
 def rectangle_builder_greater_area(a, x)
+  b = a.sort
+  c = b.reject { |n| b.count(n) == 1}
+  d = c.uniq
+  e = d.reverse
 
+  count = 0
+  for i in 0...(d.length-1)
+    for j in (i+1)...d.length
+      e[i] * e[j] >= x ? count += 1 : break
+    end
+  end
+
+  count
 end
