fa56bf40264f11482b98858878e925d3616262d2
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-16 16:01:26 +0000

    Add RectangleBuilderGreaterArea Challenge Example Test

---
+++ spec/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area"
+
+describe 'RectangleBuilderGreaterArea' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(rectangle_builder_greater_area([1,2,5,1,1,2,3,5,1], 5)).to eq 2
+    end
+  end
+end
