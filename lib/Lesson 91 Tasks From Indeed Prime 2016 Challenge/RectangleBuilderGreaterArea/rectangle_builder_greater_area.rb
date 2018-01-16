473f9dfebb383756f9ad4825a7ba17cb492a2a25
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-16 16:46:28 +0000

    Fix RectangleBuilderGreaterArea Challenge rectangle_builder_greater_area(a, x) Method Code & Pass Squares Tests

--- lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
+++ lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
@@ -3,11 +3,30 @@ def rectangle_builder_greater_area(a, x)
   c = b.reject { |n| b.count(n) == 1}
   d = c.uniq
   e = d.reverse
+  f = []
+
+  for i in 0...e.length
+    if c.count(e[i]) > 3
+      f << e[i]
+      f << e[i]
+    else
+      f << e[i]
+    end
+  end
+
+  g = []
 
   count = 0
-  for i in 0...(d.length-1)
-    for j in (i+1)...d.length
-      e[i] * e[j] >= x ? count += 1 : break
+  for i in 0...(f.length-1)
+    for j in (i+1)...f.length
+      if f[i] * f[j] >= x
+        unless g.include?([f[i],f[j]])
+          g << [f[i],f[j]]
+          count += 1
+        end
+      else
+        break
+      end
     end
   end
 
