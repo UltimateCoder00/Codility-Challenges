efc4010a00bfe82864c5e194873dfc457120fa7f
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-19 15:49:20 +0000

    Refactor PassingCars Challenge passing_cars(a) Method

--- lib/Lesson 5 Prefix Sums/PassingCars/passing_cars.rb
+++ lib/Lesson 5 Prefix Sums/PassingCars/passing_cars.rb
@@ -1,12 +1,31 @@
 def passing_cars(a)
-  for i in 0..a.length
-    a[0] == 1 ? a.shift : break
+  a = remove_redundant_elements(a)
+  count = counter(a)
+  count <= 1000000000 ? count : -1
+end
+
+def remove_redundant_elements(array)
+  array = remove_starting_1_elements(array)
+  array = remove_ending_0_elements(array)
+end
+
+def remove_starting_1_elements(array)
+  for i in 0..array.length
+    array[0] == 1 ? array.shift : break
   end
 
-  for i in 0..a.length
-    a.last == 0 ? a.pop : break
+  array
+end
+
+def remove_ending_0_elements(array)
+  for i in 0..array.length
+    array.last == 0 ? array.pop : break
   end
 
+  array
+end
+
+def counter(a)
   count = 0
   count_ones = 0
   sum = 0
@@ -21,5 +40,5 @@ def passing_cars(a)
     end
   end
 
-  count <= 1000000000 ? count : -1
+  count
 end
