444c4d3c9e127e36e69eb65877af5cfb41ad8f2b
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-19 16:25:19 +0000

    Refactor StrSymmetryPoint Challenge str_symmetry_point(s) Method

--- lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
+++ lib/Lesson 99 Future Training/StrSymmetryPoint/str_symmetry_point.rb
@@ -1,10 +1,17 @@
 def str_symmetry_point(s)
+  return -1 if not_symmetric?(s)
   return 0 if s.length == 1
-  return -1 if s.length == 0
+  s.length % 2 == 0 ? -1 : s.length / 2
+end
 
+def not_symmetric?(s)
+  s.length == 0 || broken?(s)
+end
+
+def broken?(s)
   for i in 0..(s.length / 2)
-    return -1 if s[i] != s[s.length - 1 - i]
+    return true if s[i] != s[s.length - 1 - i]
   end
 
-  s.length % 2 == 0 ? -1 : s.length / 2
+  false
 end
