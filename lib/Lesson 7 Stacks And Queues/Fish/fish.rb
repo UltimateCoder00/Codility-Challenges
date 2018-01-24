136ffc3e79bd1ee905f42f1b6090dde43a709a76
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-24 16:54:32 +0000

    Refactor Fish Challenge fish(a, b) Method Code

--- lib/Lesson 7 Stacks And Queues/Fish/fish.rb
+++ lib/Lesson 7 Stacks And Queues/Fish/fish.rb
@@ -1,43 +1,36 @@
 def fish(a, b)
   return b.count if b.uniq.count == 1
+  a, b, counting = remove_1s_at_end(a[b.index(1)..-1], b[b.index(1)..-1], b.index(1))
+  count_fish(a, b) + counting
+end
 
-  index = b.index(1)
-  extra_counting = index
-
-  a = a[index..-1]
-  b = b[index..-1]
+def remove_1s_at_end(a, b, index)
+  counting = index
 
   (b.length-1).downto(0) do |i|
-    if b[i] == 1
-      a.pop
-      b.pop
-      extra_counting += 1
-    else
-      break
-    end
+    break unless b[i] == 1
+    a.pop
+    b.pop
+    counting += 1
   end
 
+  [a, b, counting]
+end
+
+def count_fish(a, b)
   while true
     count = a.count
 
     for i in 0...a.length
-      if b[i] == 1
-        if b[i+1] == 0
-          if a[i] > a[i+1]
-            a.delete_at(i+1)
-            b.delete_at(i+1)
-          elsif a[i] < a[i+1]
-            a.delete_at(i)
-            b.delete_at(i)
-          else
-            next
-          end
-        end
-      end
+      next unless b[i] == 1 && b[i+1] == 0 && a[i] != a[i+1]
+
+      a[i] > a[i+1] ? k = i+1 : k = i
+      a.delete_at(k)
+      b.delete_at(k)
     end
 
     break if a.count == count
   end
 
-  a.count + extra_counting
+  a.count
 end
