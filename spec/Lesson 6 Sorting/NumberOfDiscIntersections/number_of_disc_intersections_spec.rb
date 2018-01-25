2c2ba7da0c9b7a51521f03e7eb6893962d390fd1
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-25 16:24:16 +0000

    Add NumberOfDiscIntersections Challenge Simple1 Test

--- spec/Lesson 6 Sorting/NumberOfDiscIntersections/number_of_disc_intersections_spec.rb
+++ spec/Lesson 6 Sorting/NumberOfDiscIntersections/number_of_disc_intersections_spec.rb
@@ -2,12 +2,21 @@ require "./lib/Lesson 6 Sorting/NumberOfDiscIntersections/number_of_disc_interse
 
 describe 'NumberOfDiscIntersections' do
   describe 'Example Tests' do
-    it 'example: [1, 5, 2, 1, 4, 0] gives 11' do
+    it 'Example 1' do
       expect(number_of_disc_intersections([1, 5, 2, 1, 4, 0])).to eq 11
     end
 
-    it 'example: [1, 10, 100, 1] gives 5' do
+    it 'Example 2' do
       expect(number_of_disc_intersections([1, 10, 100, 1])).to eq 5
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Simple1' do
+      it 'Simple' do
+        array = [3, 3, 3, 5, 1, 2]
+        expect(number_of_disc_intersections(array)).to eq 15
+      end
     end
   end
 end
