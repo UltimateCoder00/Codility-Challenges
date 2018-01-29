40ea55f155af703240786ff86669349c80480504
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-01-29 17:17:39 +0000

    Refactor RectangleBuilderGreaterArea Challenge rectangle_builder_greater_area(a, x) Method

--- lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
+++ lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/RectangleBuilderGreaterArea/rectangle_builder_greater_area.rb
@@ -1,27 +1,30 @@
 def rectangle_builder_greater_area(a, x)
   b = a.reject { |n| a.count(n) == 1}.sort
-  c = b.uniq.reverse
-  d = []
+  counter(array(b, b.uniq.reverse), x)
+end
 
+def array(b, c, d=[])
   for i in 0...c.length
     b.count(c[i]) > 3 ? d += [c[i],c[i]] : d << c[i]
   end
+  d
+end
 
-  e = []
-
-  count = 0
+def counter(d, x, e=[], count=0)
   for i in 0...(d.length-1)
     for j in (i+1)...d.length
-      if d[i] * d[j] >= x
-        unless e.include?([d[i],d[j]])
-          e << [d[i],d[j]]
-          count += 1
-        end
-      else
-        break
+      break unless above_threshold?(d[i], d[j], x)
+
+      unless e.include?([d[i],d[j]])
+        e << [d[i],d[j]]
+        count += 1
       end
     end
   end
 
   count
+end
+
+def above_threshold?(a, b, c)
+  a * b >= c
 end
