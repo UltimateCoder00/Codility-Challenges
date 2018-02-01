0effb9fcfeb8e85d1976dea8a1e02cd0d3716ea3
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-01 17:48:02 +0000

    Add MaxDoubleSliceSum Challenge Simple1 Tests

--- spec/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum_spec.rb
+++ spec/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum_spec.rb
@@ -2,9 +2,25 @@ require "./lib/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice
 
 describe 'MaxDoubleSliceSum' do
 
-  describe 'Example Tests' do
-    it 'Example: [3,2,6,-1,4,5,-1,2] to 17' do
+  describe 'Example Test' do
+    it 'Example' do
       expect(max_double_slice_sum([3,2,6,-1,4,5,-1,2])).to eq 17
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Simple1' do
+      it 'First Simple Test 1' do
+        expect(max_double_slice_sum([0,10,-5,-2,0])).to eq 10
+      end
+
+      it 'First Simple Test 2' do
+        expect(max_double_slice_sum([-2,-3,-4,1,-5,-6,-7])).to eq 1
+      end
+
+      it 'First Simple Test 3' do
+        expect(max_double_slice_sum([5,17,0,3])).to eq 17
+      end
     end
   end
 end
