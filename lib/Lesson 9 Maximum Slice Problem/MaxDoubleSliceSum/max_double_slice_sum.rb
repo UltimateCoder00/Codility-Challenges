aa192bffe2f856de639d0bcca11e69d2ce65c615
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-02 10:25:24 +0000

    Refactor MaxDoubleSliceSum Challenge max_double_slice_sum(a)

--- lib/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum.rb
+++ lib/Lesson 9 Maximum Slice Problem/MaxDoubleSliceSum/max_double_slice_sum.rb
@@ -4,14 +4,15 @@ def max_double_slice_sum(a)
   for i in 0...(a.length-2)
     for j in (i+1)...(a.length-1)
       for k in (j+1)...a.length
-        count = 0
-        count += a[(i+1)...j].inject(0){|sum,x| sum + x }
-        count += a[(j+1)...k].inject(0){|sum,x| sum + x }
-
+        count = sum_array_elements(a, i, j) + sum_array_elements(a, j, k)
         max = count if max < count
       end
     end
   end
 
   max
+end
+
+def sum_array_elements(array, i, j)
+  array[(i+1)...j].inject(0){|sum,x| sum + x }
 end
