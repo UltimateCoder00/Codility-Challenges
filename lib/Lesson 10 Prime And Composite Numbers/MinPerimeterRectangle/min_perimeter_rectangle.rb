6349cdb3ee95b2370198e595bf0224ea2c44b222
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-05 15:20:38 +0000

    Refactor MinPerimeterRectangle Challenge min_perimeter_rectangle(n) Method

--- lib/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle.rb
+++ lib/Lesson 10 Prime And Composite Numbers/MinPerimeterRectangle/min_perimeter_rectangle.rb
@@ -4,10 +4,24 @@ end
 
 def perimeter_count(n, count = 2 + 2*n)
   for i in 2...n/2
-    next unless n % i == 0
-    value = n / i
-    count = 2*i + 2*value if count > 2*i + 2*value
+    count = 2*i + 2*divide(n, i) if small_perimeter?(count, n, i)
   end
 
   count
+end
+
+def small_perimeter?(count, a, b)
+   small_perimeter_condition(count, a, b) && divisible?(a, b)
+end
+
+def small_perimeter_condition(count, a, b)
+  count > 2*b + 2*divide(a, b)
+end
+
+def divisible?(a, b)
+  a % b == 0
+end
+
+def divide(a, b)
+  a / b
 end
