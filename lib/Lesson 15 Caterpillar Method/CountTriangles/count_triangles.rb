feb5e61a09e5d2a85b85c7c65bcc80b80a5cc023
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-06 13:37:55 +0000

    Refactor CountTriangles Challenge count_triangles(a) Method

--- lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles.rb
+++ lib/Lesson 15 Caterpillar Method/CountTriangles/count_triangles.rb
@@ -1,19 +1,40 @@
 def count_triangles(a)
-  count = 0
+  triangle_plane1(a)
+end
 
+def triangle_plane1(a, count=0)
   for i in 0...(a.length-2)
-    for j in (i+1)...(a.length-1)
-      for k in (j+1)...(a.length)
-        if a[i] + a[j] > a[k]
-          if a[i] + a[k] > a[j]
-            if a[j] + a[k] > a[i]
-              count += 1
-            end
-          end
-        end
-      end
-    end
+    count += triangle_plane2(a, i)
   end
+  return count
+end
 
-  count
+def triangle_plane2(a, i, count=0)
+  for j in (i+1)...(a.length-1)
+    count += triangle_plane3(a, i, j)
+  end
+  return count
+end
+
+def triangle_plane3(a, i, j, count=0)
+  for k in (j+1)...(a.length)
+    count += 1 if triangular?(a[i], a[j], a[k])
+  end
+  return count
+end
+
+def triangular?(a, b, c)
+  triangular_condition1(a, b, c) && triangular_condition2(a, b, c) && triangular_condition3(a, b, c)
+end
+
+def triangular_condition1(a, b, c)
+  a + b > c
+end
+
+def triangular_condition2(a, b, c)
+  a + c > b
+end
+
+def triangular_condition3(a, b, c)
+  b + c > a
 end
