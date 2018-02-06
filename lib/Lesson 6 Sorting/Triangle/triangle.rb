3471fd2093a8c5b01ea5f1f14b709e72a65b287f
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-06 11:41:51 +0000

    Refactor Triangle Challenge triangle(a) Method

--- lib/Lesson 6 Sorting/Triangle/triangle.rb
+++ lib/Lesson 6 Sorting/Triangle/triangle.rb
@@ -2,16 +2,26 @@ def triangle(a)
   for i in 0...(a.length-2)
     for j in (i+1)...(a.length-1)
       for k in (j+1)...a.length
-        if a[i] + a[j] > a[k]
-          if a[j] + a[k] > a[i]
-            if a[k] + a[i] > a[j]
-              return 1
-            end
-          end
-        end
+        return 1 if triangular?(a[i], a[j], a[k])
       end
     end
   end
 
   0
+end
+
+def triangular?(a, b, c)
+  condition1(a, b, c) && condition2(a, b, c) && condition3(a, b, c)
+end
+
+def condition1(a, b, c)
+  a + b > c ? true : false
+end
+
+def condition2(a, b, c)
+  a + c > b ? true : false
+end
+
+def condition3(a, b, c)
+  b + c > a ? true : false
 end
