e9dcbed6921b1242440ddbc7403567a2a20a8459
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-07 13:18:18 +0000

    Refactor NumberOfDiscIntersections Challenge number_of_disc_intersections(a) Method

--- lib/Lesson 6 Sorting/NumberOfDiscIntersections/number_of_disc_intersections.rb
+++ lib/Lesson 6 Sorting/NumberOfDiscIntersections/number_of_disc_intersections.rb
@@ -1,30 +1,34 @@
 def number_of_disc_intersections(a)
-  count = 0
-  array1 = []
-  array2 = []
+  circle_radii = get_circle_radii(a)
+  count_intersections(a, circle_radii[0], circle_radii[1])
+end
 
+def get_circle_radii(a, left_radius=[], right_radius=[])
   a.each_with_index do |val, index|
-    array1 << index - val
-    array2 << index + val
+    left_radius << index - val
+    right_radius << index + val
   end
+  [left_radius, right_radius]
+end
 
+def count_intersections(a, array1, array2, count=0)
   for j in 0...a.length-1
     for i in j+1...a.length
-      if array1[j] <= array2[i]
-        if array2[j] >= array1[i]
-            count += 1
-        end
-      end
-
-      if array1[j] >= array2[i]
-        if array2[j] <= array1[i]
-            count += 1
-        end
-      end
-
-      return -1 if count > 10000000 
+      count += 1 if check_radii_intersection_conditions(array2[i], array1[j], array2[j], array1[i])
+      return -1 if count > 10000000
     end
   end
-
   count
+end
+
+def check_inequality(a, b)
+  a >= b
+end
+
+def check_intersection_conditions(a, b, c, d)
+  check_inequality(a, b) && check_inequality(c, d)
+end
+
+def check_radii_intersection_conditions(a, b, c, d)
+  check_intersection_conditions(a, b, c, d) || check_intersection_conditions(b, a, d, c)
 end
