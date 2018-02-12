ab87d34cf1183957272f5b434e7b52980e2f09f6
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-12 13:50:32 +0000

    Add ArrayInversionCount Challenge Simple3 Test

--- spec/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count_spec.rb
+++ spec/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count_spec.rb
@@ -20,5 +20,12 @@ describe 'ArrayInversionCount' do
         expect(array_inversion_count([10,2,1,4,1,-1,19,1])).to eq 16
       end
     end
+
+    context 'Simple3' do
+      it 'Simple' do
+        array = [181,1,1,2,4,1,1,4,6,1,2]
+        expect(array_inversion_count(array)).to eq 21
+      end
+    end
   end
 end
