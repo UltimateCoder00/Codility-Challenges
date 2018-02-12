720708e2491921a92f2eac406c07ef6ec92accf7
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-12 13:49:03 +0000

    Add ArrayInversionCount Challenge Simple1 Test

--- spec/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count_spec.rb
+++ spec/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count_spec.rb
@@ -2,9 +2,17 @@ require "./lib/Lesson 99 Future Training/ArrayInversionCount/array_inversion_cou
 
 describe 'ArrayInversionCount' do
 
-  describe 'Example Tests' do
+  describe 'Example Test' do
     it 'Example' do
       expect(array_inversion_count([-1,6,3,4,7,4])).to eq 4
+    end
+  end
+
+  describe 'Correctness Tests' do
+    context 'Simple1' do
+      it 'Simple' do
+        expect(array_inversion_count([5,4,3,2,1])).to eq 10
+      end
     end
   end
 end
