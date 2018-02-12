5801f6ac2d17293abe7400dab362a797b1ea8e2c
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-12 13:38:13 +0000

    Add ArrayInversionCount Challenge Example Test

---
+++ spec/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 99 Future Training/ArrayInversionCount/array_inversion_count"
+
+describe 'ArrayInversionCount' do
+
+  describe 'Example Tests' do
+    it 'Example' do
+      expect(array_inversion_count([-1,6,3,4,7,4])).to eq 4
+    end
+  end
+end
