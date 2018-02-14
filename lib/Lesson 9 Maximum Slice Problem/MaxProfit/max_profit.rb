9808d3ddaadff6326c97b78af93d699c27ced5b6
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-14 16:49:05 +0000

    Refactor MaxProfit Challenge max_profit(a) Method

--- lib/Lesson 9 Maximum Slice Problem/MaxProfit/max_profit.rb
+++ lib/Lesson 9 Maximum Slice Problem/MaxProfit/max_profit.rb
@@ -1,11 +1,9 @@
 def max_profit(a)
+  return 0 if a.count == 0
+
   count = 0
-
   for i in 0...(a.length-1)
-    for j in (i+1)...a.length
-      count = a[j] - a[i] if a[j] - a[i] > count
-    end
+    count = [count, a[(i+1)..-1].max - a[i]].max
   end
-
   count
 end
