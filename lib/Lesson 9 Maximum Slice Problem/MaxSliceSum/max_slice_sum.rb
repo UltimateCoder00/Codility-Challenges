144fdb5bd4a03fbf22b7cb8b829134940103771b
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-14 12:16:48 +0000

    Refactor MaxSliceSum Challenge By Adding max_slice_sum_inner_loop(a, i, count) Method

--- lib/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum.rb
+++ lib/Lesson 9 Maximum Slice Problem/MaxSliceSum/max_slice_sum.rb
@@ -1,12 +1,16 @@
 def max_slice_sum(a)
   count = a[0]
   for i in 0...a.length
-    for j in i...a.length
-      array_sum = array_sum(a, i, j)
-      count = array_sum if count < array_sum
-    end
+    count = max_slice_sum_inner_loop(a, i, count)
   end
+  count
+end
 
+def max_slice_sum_inner_loop(a, i, count)
+  for j in i...a.length
+    array_sum = array_sum(a, i, j)
+    count = array_sum if count < array_sum
+  end
   count
 end
 
