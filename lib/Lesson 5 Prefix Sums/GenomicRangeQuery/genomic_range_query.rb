a164bb8d12c404d42265ef78b65138e9263bdd4a
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-19 16:17:25 +0000

    Refactor GenomicRangeQuery Challenge genomic_range_query(s, p, q) Method

--- lib/Lesson 5 Prefix Sums/GenomicRangeQuery/genomic_range_query.rb
+++ lib/Lesson 5 Prefix Sums/GenomicRangeQuery/genomic_range_query.rb
@@ -1,24 +1,14 @@
 def genomic_range_query(s, p, q)
-  impact_factor_array(turn_into_array_integers(replace_characters(s)), p, q)
-end
-
-def replace_characters(array)
-  replacements = [ ["A","1"], ["C","2"], ["G","3"], ["T","4"] ]
-  replacements.each {|replacement| array.gsub!(replacement[0], replacement[1])}
-  array
-end
-
-def turn_into_array_integers(array)
-  array.split("").map(&:to_i)
-end
-
-def impact_factor_array(s, p, q, array=[])
+  array = []
   for i in 0...p.length
-    [1,2,3,4].each do |val|
-      if s[p[i]..q[i]].include?(val)
-        array << val
-        break
-      end
+    if s[p[i]..q[i]].include?("A")
+      array << 1
+    elsif s[p[i]..q[i]].include?("C")
+      array << 2
+    elsif s[p[i]..q[i]].include?("G")
+      array << 3
+    else
+      array << 4
     end
   end
   array
