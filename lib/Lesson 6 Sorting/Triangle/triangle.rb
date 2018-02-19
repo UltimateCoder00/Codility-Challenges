5141469c5c24d7cdd8b8089a6a9399a5387e3c3e
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-19 14:56:05 +0000

    Refactor Triangle Challenge triangle(a) Method

--- lib/Lesson 6 Sorting/Triangle/triangle.rb
+++ lib/Lesson 6 Sorting/Triangle/triangle.rb
@@ -1,22 +1,10 @@
 def triangle(a)
-  triangle_plane1(a) == 1 ? 1 : 0
+  triangle_plane(a.sort) == 1 ? 1 : 0
 end
 
-def triangle_plane1(a)
+def triangle_plane(a)
   for i in 0...(a.length-2)
-    return 1 if triangle_plane2(a, i) == 1
-  end
-end
-
-def triangle_plane2(a, i)
-  for j in (i+1)...(a.length-1)
-    return 1 if triangle_plane3(a, i, j) == 1
-  end
-end
-
-def triangle_plane3(a, i, j)
-  for k in (j+1)...a.length
-    return 1 if triangular?(a[i], a[j], a[k])
+    return 1 if triangular?(a[i], a[i+1], a[i+2])
   end
 end
 
