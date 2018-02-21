717c79132e3dd06c478f1102ab56588c429be170
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-21 00:01:25 +0000

    Refactor FrogRiverOne Challenge frog_river_one(x, a) Method

--- lib/Lesson 4 Counting Elements/FrogRiverOne/frog_river_one.rb
+++ lib/Lesson 4 Counting Elements/FrogRiverOne/frog_river_one.rb
@@ -1,6 +1,8 @@
 def frog_river_one(x, a)
   array = a.uniq
-  array2 = array.sort
+  identical?(x, array) ? a.index(array.last) : -1
+end
 
-  [*1..x] == array2 ? a.index(array.last) : -1
+def identical?(x, array)
+  [*1..x] == array.sort
 end
