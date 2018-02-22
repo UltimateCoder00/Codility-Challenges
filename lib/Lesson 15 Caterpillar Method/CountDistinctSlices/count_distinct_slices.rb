febd62d0185f34a0f87dd845b2a6bc716fb31ed9
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-22 17:03:36 +0000

    Refactor CountDistinctSlices Challenge By Adding duplicates?(array)

--- lib/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices.rb
+++ lib/Lesson 15 Caterpillar Method/CountDistinctSlices/count_distinct_slices.rb
@@ -3,9 +3,13 @@ def count_distinct_slices(m, a)
 
   for i in 0...a.length
     for j in i...a.length
-      a[i..j].uniq == a[i..j] ? count += 1 : break
+      duplicates?(a[i..j]) ? count += 1 : break
     end
   end
 
   count < 1000000000 ? count : 1000000000
+end
+
+def duplicates?(array)
+  array.uniq == array
 end
