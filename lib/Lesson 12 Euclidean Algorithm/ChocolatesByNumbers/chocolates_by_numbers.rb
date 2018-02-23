b617a808126723f6f66bdb0d38766ac10df9375e
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-23 16:05:59 +0000

    Refactor ChocolatesByNumbers Challenge chocolates_by_numbers(n, m) Method

--- lib/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers.rb
+++ lib/Lesson 12 Euclidean Algorithm/ChocolatesByNumbers/chocolates_by_numbers.rb
@@ -1,19 +1,12 @@
 def chocolates_by_numbers(n, m)
-  count = 0
-  return 1 if n == 1
-  return n if m == 1
-
+  chocolate_count = i = 0
   array = []
-  i = 0
 
   loop do
     array << i
-    count += 1
     division = (n - i - 1) / m
-    count += division
+    chocolate_count += division + 1
     i = ( division*m + m + i ) % n
-    break if array.include?(i)
+    return chocolate_count if array.include?(i)
   end
-
-  count
 end
