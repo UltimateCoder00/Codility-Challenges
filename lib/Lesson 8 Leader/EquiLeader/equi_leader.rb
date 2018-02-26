124c4e4858e494addd6d5c3398c89cabf69ce200
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-26 16:51:41 +0000

    Refactor EquiLeader Challenge dominator(a) Method

--- lib/Lesson 8 Leader/EquiLeader/equi_leader.rb
+++ lib/Lesson 8 Leader/EquiLeader/equi_leader.rb
@@ -2,28 +2,28 @@ def equi_leader(a)
   count = 0
 
   for i in 0...(a.length-1)
-    next if dominator(a[0..i]) == -1
-    next if dominator(a[i+1..-1]) == -1
-
-    if dominator(a[0..i]) == dominator(a[i+1..-1])
-      count += 1
-    end
+    part1 = dominator(a[0..i])
+    next if part1 == -1
+    part2 = dominator(a[i+1..-1])
+    next if part2 == -1
+    count += 1 if part1 == part2
   end
 
   count
 end
 
 def dominator(a)
-  return -1 if a.count < 1
-  b = a.uniq.sort
-  c = []
+  b = [a.first]
 
-  for i in 0...b.length
-    c << a.count(b[i])
+  for i in 1...a.length
+    b = new_array(b)
+    b << a[i]
   end
 
-  return -1 if c.count(c.max) > 1
-  return -1 if a.count(b[c.index(c.max)]) <= a.length / 2
+  a.count(b.first) > (a.length / 2) ? b.first : -1
+end
 
-  a[a.index(b[c.index(c.max)])]
+def new_array(b)
+  return b[0..-3] if b[-1] != b[-2] unless b.length <= 1
+  b
 end
