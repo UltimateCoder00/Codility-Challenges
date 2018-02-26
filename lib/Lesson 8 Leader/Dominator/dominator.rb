3944fd1e6ba5d408b85714d9133c9ebb0c1924a7
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-26 16:04:17 +0000

    Refactor Dominator Challenge dominator(a) Method By Adding new_array(b) Method

--- lib/Lesson 8 Leader/Dominator/dominator.rb
+++ lib/Lesson 8 Leader/Dominator/dominator.rb
@@ -2,9 +2,14 @@ def dominator(a)
   b = [a.first]
 
   for i in 1...a.length
-    b = b[0..-3] if b[-1] != b[-2] unless b.length <= 1
+    b = new_array(b)
     b << a[i]
   end
 
   a.count(b.first) > (a.length / 2) ? a.index(b.first) : -1
+end
+
+def new_array(b)
+  return b[0..-3] if b[-1] != b[-2] unless b.length <= 1
+  b
 end
