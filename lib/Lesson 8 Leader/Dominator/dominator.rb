60072e32f04508d145cc6c12edf9034fcba8aa32
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-26 15:17:36 +0000

    Refactor Dominator Challenge dominator(a) Method

--- lib/Lesson 8 Leader/Dominator/dominator.rb
+++ lib/Lesson 8 Leader/Dominator/dominator.rb
@@ -1,14 +1,6 @@
 def dominator(a)
-  return -1 if a.count < 1
-  b = a.uniq.sort
-  c = []
-
-  for i in 0...b.length
-    c << a.count(b[i])
-  end
-
-  return -1 if c.count(c.max) > 1
-  return -1 if a.count(b[c.index(c.max)]) <= a.length / 2
-
-  a.index(b[c.index(c.max)])
+  max_number = a.max_by { |x| a.count(x) }
+  max_number_count = a.count(max_number)
+  b = a.reject { |x| x == max_number }
+  max_number_count > b.length ? a.index(max_number) : -1
 end
