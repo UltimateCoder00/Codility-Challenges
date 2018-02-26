9ce68be5a21f868016c2e9407670d898d9725f2c
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-02-26 15:54:39 +0000

    Refactor Dominator Challenge dominator(a) Method

--- lib/Lesson 8 Leader/Dominator/dominator.rb
+++ lib/Lesson 8 Leader/Dominator/dominator.rb
@@ -1,21 +1,11 @@
 def dominator(a)
-  return 0 if a.count == 1
-  return -1 if a.count == 2
-
   b = [a.first]
 
   for i in 1...a.length
-    if b.length <= 1
-      b << a[i]
-    else
-      if b[-1] != b[-2]
-        b.pop
-        b.pop
-      end
-      b << a[i]
-    end
+    b = b[0..-3] if b[-1] != b[-2] unless b.length <= 1
+    b << a[i]
   end
 
   max_number = b.first
-  a.count(max_number) > a.length/2 ? a.index(max_number) : -1
+  a.count(max_number) > (a.length / 2) ? a.index(max_number) : -1
 end
