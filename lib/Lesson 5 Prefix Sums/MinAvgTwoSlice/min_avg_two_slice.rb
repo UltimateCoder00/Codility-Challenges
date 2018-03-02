f0d5bd224ef8bf767c3fd748c121394f147847d9
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-02 11:53:39 +0000

    Refactor MinAvgTwoSlice Challenge min_average?(sum, i, j, min_average) Method

--- lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/min_avg_two_slice.rb
+++ lib/Lesson 5 Prefix Sums/MinAvgTwoSlice/min_avg_two_slice.rb
@@ -23,23 +23,9 @@ def average(sum, i, j)
 end
 
 def min_average?(sum, i, j, min_average)
-  sum / distance(i, j) < min_average
+  average(sum, i, j) < min_average
 end
 
 def distance(i, j)
   (j - i + 1.0)
-end
-
-def method2(a)
-  array = []
-  array2 = []
-
-  comb = [*0...a.length].combination(2).to_a
-
-  comb.each do |x|
-    array << ( a[x[0]..x[1]].sum / (x[1] - x[0] + 1.0) )
-    array2 << x[0]
-  end
-
-  [array, array2]
 end
