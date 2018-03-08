c98f6a030235b60b5305ba5f172a53a6a66f5522
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-08 16:36:10 +0000

    Add MinAbsSum Challenge Medium1 Test

--- spec/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum_spec.rb
+++ spec/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum_spec.rb
@@ -48,11 +48,11 @@ describe 'MinAbsSum' do
     end
 
     context 'Range' do
-      it 'range 2..20' do
+      it 'Range 2..20' do
         expect(min_abs_sum([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])).to eq 1
       end
 
-      it 'range 10..14' do
+      it 'Range 10..14' do
         expect(min_abs_sum([10,11,12,13,14])).to eq 6
       end
     end
@@ -62,7 +62,7 @@ describe 'MinAbsSum' do
         expect(min_abs_sum([])).to eq 0
       end
 
-      it 'Single element' do
+      it 'Single Element' do
         expect(min_abs_sum([7])).to eq 7
       end
     end
@@ -78,6 +78,26 @@ describe 'MinAbsSum' do
 
       it 'Small Functional Test 3' do
         expect(min_abs_sum([91,92,93,94,95,96,97])).to eq 82
+      end
+    end
+  end
+
+  describe 'Performance Test' do
+    context 'Medium1' do
+      it 'Medium Random 1' do
+        array = [16, -61, 94, 85, -7, 33, -57, -56, -43, 39, -58, 95, -86, -62,
+                  -83, 54, -27, -6, -35, 28, -19, -59, -14, -39, -78, 65, 36,
+                  33, 77, -21, -24, -15, -19, 34, -99, -7, 7, -31, -96, 41, -99,
+                  13, -70, -94, -64, -87, -70, 37, 46, -28, 31, -5, 0, 76, -38,
+                  74, 38, 37, -99, -84, -94, -31, 90, 76, 68, -77, 41, 55, 62,
+                  59, -72, -65, 59, 48, 79, -11, 78, -72, 92, 77, -33, -87, 10,
+                  38, -30, -89, -44, 65, -82, 88, -50, -59, -78, 97, 81, -36,
+                  -81, 31, 78, -98]
+        expect(min_abs_sum(array)).to eq 0
+      end
+
+      it 'Medium Random 2' do
+        expect(min_abs_sum([2,2,1])).to eq 1
       end
     end
   end
