949061440aff6b2ad014630930061307df32a818
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-08 15:42:48 +0000

    Add MinAbsSum Challenge Simple1 Tests

--- spec/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum_spec.rb
+++ spec/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum_spec.rb
@@ -7,4 +7,20 @@ describe 'MinAbsSum' do
       expect(min_abs_sum([1,5,2,-2])).to eq 0
     end
   end
+
+  describe 'Correctness Test' do
+    context 'Simple1' do
+      it 'Simple1 1' do
+        expect(min_abs_sum([1,5,-2,5,2,3])).to eq 0
+      end
+
+      it 'Simple1 2' do
+        expect(min_abs_sum([2,2,1])).to eq 1
+      end
+
+      it 'Simple1 3' do
+        expect(min_abs_sum([3,1])).to eq 2
+      end
+    end
+  end
 end
