a16b951b046a98c666d17c54bbfd9d77061fec5a
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-08 15:32:06 +0000

    Add MinAbsSum Challenge min_abs_sum(a) Method Code

--- lib/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum.rb
+++ lib/Lesson 17 Dynamic Programming/MinAbsSum/min_abs_sum.rb
@@ -1,3 +1,13 @@
 def min_abs_sum(a)
+  min_abs_sum = a.first
 
+  for i in 1...a.length
+    if min_abs_sum > 0
+      a[i] > 0 ? min_abs_sum -= a[i] : min_abs_sum += a[i]
+    else
+      a[i] > 0 ? min_abs_sum += a[i] : min_abs_sum -= a[i]
+    end
+  end
+
+  min_abs_sum
 end
