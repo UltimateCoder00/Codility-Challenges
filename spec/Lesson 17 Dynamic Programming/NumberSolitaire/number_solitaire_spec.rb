3f6c362203bb474db74a1c2e7a212e119d04a3c0
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-13 17:00:03 +0000

    Add NumberSolitaire Challenge Example Test

--- spec/Lesson 17 Dynamic Programming/NumberSolitaire/number_solitaire_spec.rb
+++ spec/Lesson 17 Dynamic Programming/NumberSolitaire/number_solitaire_spec.rb
@@ -1,3 +1,10 @@
-def number_solitaire(a)
+require "./lib/Lesson 17 Dynamic Programming/NumberSolitaire/number_solitaire"
 
+describe 'NumberSolitaire' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(number_solitaire([1,-2,0,9,-1,-2])).to eq 8
+    end
+  end
 end
