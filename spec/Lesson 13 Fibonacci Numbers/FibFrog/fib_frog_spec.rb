7496de5494e36f5e4364e1630f86fef35d9e465e
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-15 13:30:08 +0000

    Add FibFrog Challenge Example Test

---
+++ spec/Lesson 13 Fibonacci Numbers/FibFrog/fib_frog_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 13 Fibonacci Numbers/FibFrog/fib_frog"
+
+describe 'FibFrog' do
+
+  describe 'Example Tests' do
+    it 'Example ' do
+      expect(fib_frog([0,0,0,1,1,0,1,0,0,0,0])).to eq 3
+    end
+  end
+end
