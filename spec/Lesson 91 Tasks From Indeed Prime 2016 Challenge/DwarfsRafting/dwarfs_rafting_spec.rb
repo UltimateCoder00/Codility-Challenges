87e70d2e585320802c3baf1360b7291c0620f466
ultimatecoder00 <hasansonmez100@hotmail.com>
2018-03-21 10:31:02 +0000

    Add DwarfsRafting Challenge Example Test

---
+++ spec/Lesson 91 Tasks From Indeed Prime 2016 Challenge/DwarfsRafting/dwarfs_rafting_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/Lesson 91 Tasks From Indeed Prime 2016 Challenge/DwarfsRafting/dwarfs_rafting"
+
+describe 'DwarfsRafting' do
+
+  describe 'Example Test' do
+    it 'Example' do
+      expect(dwarfs_rafting(4, "1B 1C 4B 1D 2A", "3B 2D")).to eq 6
+    end
+  end
+end
