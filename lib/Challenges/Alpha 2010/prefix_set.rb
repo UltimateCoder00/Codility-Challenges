afdc8e6aa691f51898bc2e4bd5c0d3a90b8ec085
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-08-07 17:53:22 +0100

    Refactor Alpha 2010 Challenge prefix_set Method Code

--- lib/Challenges/Alpha 2010/prefix_set.rb
+++ lib/Challenges/Alpha 2010/prefix_set.rb
@@ -1,7 +1,18 @@
 def prefix_set(array)
+  return 0 if array.uniq.length == 1
   unique_array = array.uniq.sort
 
-  ((unique_array.length - 1)..array.length).each do |index|
-    return index if array[0..index].uniq.sort == unique_array
+  if unique_array.length < array.length / 10
+    ((unique_array.length - 1)..array.length).each do |index|
+      return index if array[0..index].uniq.sort == unique_array
+    end
+  else
+    (array.length - 1).downto(0) do |index|
+      if array.count(array[index]) == 1
+        return index
+      else
+        array.pop
+      end
+    end
   end
 end
