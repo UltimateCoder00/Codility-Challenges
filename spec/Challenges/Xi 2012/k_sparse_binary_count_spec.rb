6324c24c3457f35e5d335946580bd2a15b70c3cb
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-08-15 20:53:27 +0100

    Add KSparseBinaryCount Challenge Example Test

---
+++ spec/Challenges/Xi 2012/k_sparse_binary_count_spec.rb
@@ -0,0 +1,11 @@
+require './lib/Challenges/Xi 2012/k_sparse_binary_count'
+
+describe 'KSparseBinaryCount' do
+  describe 'Example Test' do
+    context 'Example' do
+      it 'Returns Result For Example Test' do
+        expect(k_sparse_binary_count('101', '1111', 2)).to eq 2
+      end
+    end
+  end
+end
